0614a8589cdbc85c0021724819cac0dded7914d5
Alexdoru <57050655+alexdoru@users.noreply.github.com>
2021-11-05 01:27:28 +0100

    ajouté config pour bouger l'hud de strength hunter

--- src/main/java/fr/alexdoru/megawallsenhancementsmod/gui/HunterStrengthGui.java
+++ src/main/java/fr/alexdoru/megawallsenhancementsmod/gui/HunterStrengthGui.java
@@ -1,17 +1,20 @@
 package fr.alexdoru.megawallsenhancementsmod.gui;
 
+import fr.alexdoru.megawallsenhancementsmod.config.MWEnConfigHandler;
 import net.minecraft.client.gui.ScaledResolution;
 
 public class HunterStrengthGui extends MyCachedGui {
 
     public static HunterStrengthGui instance;
 
+    private static final String DUMMY_TEXT = "\u00a77(\u00a7l\u00a7c\u00a7lStrength\u00a77) \u00a7e\u00a7l10";
     private static long timeStartRender;
     private static final double xd = 0.5d;
     private static final double yd = 8d / 20d;
 
     public HunterStrengthGui() {
         instance = this;
+        guiPosition = MWEnConfigHandler.hunterStrengthHUDPosition;
     }
 
     @Override
@@ -20,6 +23,15 @@ public class HunterStrengthGui extends MyCachedGui {
         int x = (int) (xd * res.getScaledWidth());
         int y = (int) (yd * res.getScaledHeight());
         drawCenteredString(frObj, displayText, x, y, 0);
+    }
+
+    @Override
+    public void renderDummy() {
+        super.renderDummy();
+        int[] absolutePos = this.guiPosition.getAbsolutePosition();
+        int x = absolutePos[0];
+        int y = absolutePos[1];
+        drawCenteredString(frObj, DUMMY_TEXT, x, y, 0);
     }
 
     @Override
