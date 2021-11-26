e8d778d6726bdcddd1fbcc5613549927c909f2cf
Alexdoru <alex.dolley@free.fr>
2021-11-26 17:01:39 +0100

    commencé à ajouter des trucs

---
+++ src/main/java/fr/alexdoru/megawallsenhancementsmod/data/MWGameInfo.java
@@ -0,0 +1,7 @@
+package fr.alexdoru.megawallsenhancementsmod.data;
+
+public class MWGameInfo {
+
+    // TODO mettre le game id, et l'utiliser pour le fk counter ?
+    // TODO liste des joueurs dans la game
+}
---
+++ src/main/java/fr/alexdoru/megawallsenhancementsmod/data/MWPlayer.java
@@ -0,0 +1,12 @@
+package fr.alexdoru.megawallsenhancementsmod.data;
+
+import fr.alexdoru.nocheatersmod.data.WDR;
+
+public class MWPlayer {
+
+    private String squadname;
+    private String extraprefix;
+    private String suffix;
+    private WDR wdr;
+
+}
