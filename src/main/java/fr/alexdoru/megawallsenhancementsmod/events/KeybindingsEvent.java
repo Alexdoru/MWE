3b87bc7867a510f4dfc0f79ce7f52d38e715b2fe
Alexdoru <57050655+alexdoru@users.noreply.github.com>
2021-11-28 20:14:53 +0100

    removed logglitching

--- src/main/java/fr/alexdoru/megawallsenhancementsmod/events/KeybindingsEvent.java
+++ src/main/java/fr/alexdoru/megawallsenhancementsmod/events/KeybindingsEvent.java
@@ -1,18 +1,13 @@
 package fr.alexdoru.megawallsenhancementsmod.events;
 
-import fr.alexdoru.megawallsenhancementsmod.MegaWallsEnhancementsMod;
-import fr.alexdoru.megawallsenhancementsmod.misc.LogGlitching;
 import fr.alexdoru.nocheatersmod.NoCheatersMod;
 import fr.alexdoru.nocheatersmod.commands.CommandWDR;
 import net.minecraft.client.Minecraft;
-import net.minecraftforge.client.ClientCommandHandler;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent.KeyInputEvent;
 import org.lwjgl.input.Keyboard;
 
 public class KeybindingsEvent {
-
-    private final LogGlitching logGlitching = new LogGlitching();
 
     @SubscribeEvent
     public void key(KeyInputEvent e) {
@@ -26,13 +21,7 @@ public class KeybindingsEvent {
                 if (Keyboard.getEventKeyState()) {
                     int keycode = Keyboard.getEventKey();
 
-                    if (keycode == MegaWallsEnhancementsMod.log_key_fast.getKeyCode()) {
-                        logGlitching.fastlogglitch();
-                    } else if (keycode == MegaWallsEnhancementsMod.log_key_normal.getKeyCode()) {
-                        logGlitching.logglitch();
-                    } else if (keycode == MegaWallsEnhancementsMod.killkey.getKeyCode()) {
-                        ClientCommandHandler.instance.executeCommand(Minecraft.getMinecraft().thePlayer, "/kill");
-                    } else if (keycode == NoCheatersMod.addtimemark_key.getKeyCode()) {
+                    if (keycode == NoCheatersMod.addtimemark_key.getKeyCode()) {
                         CommandWDR.addTimeMark();
                     }
 
