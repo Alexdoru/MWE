95c7ee0a39f52cb7da0dfc7359a4514fa0ab586c
Alexdoru <57050655+alexdoru@users.noreply.github.com>
2021-11-28 20:14:21 +0100

    delete unused classes

--- src/main/java/fr/alexdoru/megawallsenhancementsmod/data/MWGameInfo.java
+++
@@ -1,7 +0,0 @@
-package fr.alexdoru.megawallsenhancementsmod.data;
-
-public class MWGameInfo {
-
-    // TODO mettre le game id, et l'utiliser pour le fk counter ?
-    // TODO liste des joueurs dans la game
-}
