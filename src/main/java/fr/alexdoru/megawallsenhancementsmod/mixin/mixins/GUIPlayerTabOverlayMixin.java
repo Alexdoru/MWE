8718427566c2088ac4c060d480221b58162b2a40
Alexdoru <57050655+alexdoru@users.noreply.github.com>
2021-12-30 01:46:30 +0100

    redone the TabScoresMixins
    (cherry picked from commit 01f3ca8ef5e559964475500e331d516f369e1f80)

--- src/main/java/fr/alexdoru/megawallsenhancementsmod/mixin/mixins/GUIPlayerTabOverlayMixin.java
+++ src/main/java/fr/alexdoru/megawallsenhancementsmod/mixin/mixins/GUIPlayerTabOverlayMixin.java
@@ -4,17 +4,17 @@ import fr.alexdoru.fkcountermod.FKCounterMod;
 import fr.alexdoru.megawallsenhancementsmod.config.ConfigHandler;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
-import net.minecraft.client.gui.GuiIngame;
 import net.minecraft.client.gui.GuiPlayerTabOverlay;
 import net.minecraft.client.network.NetworkPlayerInfo;
-import net.minecraft.scoreboard.IScoreObjectiveCriteria;
 import net.minecraft.scoreboard.ScoreObjective;
 import net.minecraft.util.EnumChatFormatting;
-import net.minecraft.util.MathHelper;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import static org.objectweb.asm.Opcodes.GETSTATIC;
 
 @Mixin(GuiPlayerTabOverlay.class)
 public class GUIPlayerTabOverlayMixin extends Gui {
@@ -22,13 +22,6 @@ public class GUIPlayerTabOverlayMixin extends Gui {
     @Final
     @Shadow
     private Minecraft mc;
-
-    @Final
-    @Shadow
-    private GuiIngame guiIngame;
-
-    @Shadow
-    private long lastTimeOpened;
 
     // Replaced by patcher 1.7.0
     ///**
@@ -61,116 +54,41 @@ public class GUIPlayerTabOverlayMixin extends Gui {
      * @author Alexdoru
      * @reason adds colors to the scoreboard values, assuming the values are health of players it will draw them with colors according to your max health
      */
-    @Overwrite
-    private void drawScoreboardValues(ScoreObjective p_175247_1_, int p_175247_2_, String p_175247_3_, int p_175247_4_, int p_175247_5_, NetworkPlayerInfo p_175247_6_) {
-        int i = p_175247_1_.getScoreboard().getValueFromObjective(p_175247_3_, p_175247_1_).getScorePoints();
+    @Redirect(
+            method = "drawScoreboardValues",
+            at = @At(value = "FIELD",
+                    target = "Lnet/minecraft/util/EnumChatFormatting;YELLOW:Lnet/minecraft/util/EnumChatFormatting;",
+                    opcode = GETSTATIC)
+    )
+    private EnumChatFormatting addColorsToScoreboardValues(ScoreObjective p_175247_1_, int p_175247_2_, String p_175247_3_, int p_175247_4_, int p_175247_5_, NetworkPlayerInfo p_175247_6_) {
 
-        if (p_175247_1_.getRenderType() == IScoreObjectiveCriteria.EnumRenderType.HEARTS) {
-            this.mc.getTextureManager().bindTexture(net.minecraft.client.gui.Gui.icons);
+        if (ConfigHandler.useColoredScores) {
 
-            if (this.lastTimeOpened == p_175247_6_.func_178855_p()) {
-                if (i < p_175247_6_.func_178835_l()) {
-                    p_175247_6_.func_178846_a(Minecraft.getSystemTime());
-                    p_175247_6_.func_178844_b(this.guiIngame.getUpdateCounter() + 20);
-                } else if (i > p_175247_6_.func_178835_l()) {
-                    p_175247_6_.func_178846_a(Minecraft.getSystemTime());
-                    p_175247_6_.func_178844_b(this.guiIngame.getUpdateCounter() + 10);
-                }
-            }
+            float maxhealth;
+            float playerhealth = mc.thePlayer.getMaxHealth();
 
-            if (Minecraft.getSystemTime() - p_175247_6_.func_178847_n() > 1000L || this.lastTimeOpened != p_175247_6_.func_178855_p()) {
-                p_175247_6_.func_178836_b(i);
-                p_175247_6_.func_178857_c(i);
-                p_175247_6_.func_178846_a(Minecraft.getSystemTime());
-            }
-
-            p_175247_6_.func_178843_c(this.lastTimeOpened);
-            p_175247_6_.func_178836_b(i);
-            int j = MathHelper.ceiling_float_int((float) Math.max(i, p_175247_6_.func_178860_m()) / 2.0F);
-            int k = Math.max(MathHelper.ceiling_float_int((float) (i / 2)), Math.max(MathHelper.ceiling_float_int((float) (p_175247_6_.func_178860_m() / 2)), 10));
-            boolean flag = p_175247_6_.func_178858_o() > (long) this.guiIngame.getUpdateCounter() && (p_175247_6_.func_178858_o() - (long) this.guiIngame.getUpdateCounter()) / 3L % 2L == 1L;
-
-            if (j > 0) {
-                float f = Math.min((float) (p_175247_5_ - p_175247_4_ - 4) / (float) k, 9.0F);
-
-                if (f > 3.0F) {
-                    for (int l = j; l < k; ++l) {
-                        this.drawTexturedModalRect((float) p_175247_4_ + (float) l * f, (float) p_175247_2_, flag ? 25 : 16, 0, 9, 9);
-                    }
-
-                    for (int j1 = 0; j1 < j; ++j1) {
-                        this.drawTexturedModalRect((float) p_175247_4_ + (float) j1 * f, (float) p_175247_2_, flag ? 25 : 16, 0, 9, 9);
-
-                        if (flag) {
-                            if (j1 * 2 + 1 < p_175247_6_.func_178860_m()) {
-                                this.drawTexturedModalRect((float) p_175247_4_ + (float) j1 * f, (float) p_175247_2_, 70, 0, 9, 9);
-                            }
-
-                            if (j1 * 2 + 1 == p_175247_6_.func_178860_m()) {
-                                this.drawTexturedModalRect((float) p_175247_4_ + (float) j1 * f, (float) p_175247_2_, 79, 0, 9, 9);
-                            }
-                        }
-
-                        if (j1 * 2 + 1 < i) {
-                            this.drawTexturedModalRect((float) p_175247_4_ + (float) j1 * f, (float) p_175247_2_, j1 >= 10 ? 160 : 52, 0, 9, 9);
-                        }
-
-                        if (j1 * 2 + 1 == i) {
-                            this.drawTexturedModalRect((float) p_175247_4_ + (float) j1 * f, (float) p_175247_2_, j1 >= 10 ? 169 : 61, 0, 9, 9);
-                        }
-                    }
-                } else {
-                    float f1 = MathHelper.clamp_float((float) i / 20.0F, 0.0F, 1.0F);
-                    int i1 = (int) ((1.0F - f1) * 255.0F) << 16 | (int) (f1 * 255.0F) << 8;
-                    String s = "" + (float) i / 2.0F;
-
-                    if (p_175247_5_ - this.mc.fontRendererObj.getStringWidth(s + "hp") >= p_175247_4_) {
-                        s = s + "hp";
-                    }
-
-                    this.mc.fontRendererObj.drawStringWithShadow(s, (float) ((p_175247_5_ + p_175247_4_) / 2 - this.mc.fontRendererObj.getStringWidth(s) / 2), (float) p_175247_2_, i1);
-                }
-            }
-        } else {
-            /* ***************** MODIFIED WITH MIXINS - START ******************/
-
-            // TODO changer plutot que d'override toute la methode, ASM ou mixin
-
-            String s1;
-
-            if (ConfigHandler.useColoredScores) {
-
-                float maxhealth;
-                float playerhealth = mc.thePlayer.getMaxHealth();
-
-                if (FKCounterMod.isInMwGame() && playerhealth == 20f) {
-                    maxhealth = 40f;
-                } else {
-                    maxhealth = playerhealth;
-                }
-
-                float ifloat = (float) i;
-
-                if (ifloat > maxhealth) {
-                    s1 = EnumChatFormatting.DARK_GREEN.toString();
-                } else if (ifloat > maxhealth * 3 / 4) {
-                    s1 = EnumChatFormatting.GREEN.toString();
-                } else if (ifloat > maxhealth / 2) {
-                    s1 = EnumChatFormatting.YELLOW.toString();
-                } else if (ifloat > maxhealth / 4) {
-                    s1 = EnumChatFormatting.RED.toString();
-                } else {
-                    s1 = EnumChatFormatting.DARK_RED.toString();
-                }
-
-                s1 += i;
-
+            if (FKCounterMod.isInMwGame() && playerhealth == 20f) {
+                maxhealth = 40f;
             } else {
-                s1 = EnumChatFormatting.YELLOW.toString() + i;
+                maxhealth = playerhealth;
             }
 
-            /* ***************** MODIFIED WITH MIXINS - END ********************/
-            this.mc.fontRendererObj.drawStringWithShadow(s1, (float) (p_175247_5_ - this.mc.fontRendererObj.getStringWidth(s1)), (float) p_175247_2_, 16777215);
+            float ifloat = (float) p_175247_1_.getScoreboard().getValueFromObjective(p_175247_3_, p_175247_1_).getScorePoints();
+
+            if (ifloat > maxhealth) {
+                return EnumChatFormatting.DARK_GREEN;
+            } else if (ifloat > maxhealth * 3 / 4) {
+                return EnumChatFormatting.GREEN;
+            } else if (ifloat > maxhealth / 2) {
+                return EnumChatFormatting.YELLOW;
+            } else if (ifloat > maxhealth / 4) {
+                return EnumChatFormatting.RED;
+            } else {
+                return EnumChatFormatting.DARK_RED;
+            }
+
+        } else {
+            return EnumChatFormatting.YELLOW;
         }
 
     }
