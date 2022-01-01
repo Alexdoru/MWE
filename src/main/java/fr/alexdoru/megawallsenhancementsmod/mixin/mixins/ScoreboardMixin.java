2b1a43431c73ca5a00c725d203a39e32362e2903
Alexdoru <alex.dolley@free.fr>
2022-01-01 22:53:25 +0100

    remis le mixin pour transformer les noms
    (cherry picked from commit 2a20652b5dee82affb3d2bb394b7768eeebdcc47)

--- src/main/java/fr/alexdoru/megawallsenhancementsmod/mixin/mixins/ScoreboardMixin.java
+++ src/main/java/fr/alexdoru/megawallsenhancementsmod/mixin/mixins/ScoreboardMixin.java
@@ -1,36 +1,36 @@
-//package fr.alexdoru.megawallsenhancementsmod.mixin.mixins;
-//
-//import fr.alexdoru.megawallsenhancementsmod.utils.NameUtil;
-//import net.minecraft.scoreboard.ScorePlayerTeam;
-//import net.minecraft.scoreboard.Scoreboard;
-//import org.spongepowered.asm.mixin.Mixin;
-//import org.spongepowered.asm.mixin.injection.At;
-//import org.spongepowered.asm.mixin.injection.Inject;
-//import org.spongepowered.asm.mixin.injection.Slice;
-//import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-//import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-//
-//@Mixin(Scoreboard.class)
-//public class ScoreboardMixin {
-//
-//    @Inject(
-//            method = "addPlayerToTeam",
-//            slice = @Slice(from = @At(value = "INVOKE", target = "Lnet/minecraft/scoreboard/ScorePlayerTeam;getMembershipCollection()Ljava/util/Collection;")),
-//            at = @At(value = "RETURN")
-//    )
-//    public void addPlayerToTeam(String player, String newTeam, CallbackInfoReturnable<Boolean> ci) {
-//        if (ci.getReturnValueZ()) NameUtil.transformNameTablist(player);
-//    }
-//
-//    @Inject(
-//            method = "removePlayerFromTeam",
-//            at = @At(
-//                    value = "INVOKE",
-//                    target = "Ljava/util/Collection;remove(Ljava/lang/Object;)Z",
-//                    shift = At.Shift.AFTER
-//            ))
-//    public void removePlayerFromTeam(String p_96512_1_, ScorePlayerTeam p_96512_2_, CallbackInfo ci) {
-//        NameUtil.transformNameTablist(p_96512_1_);
-//    }
-//
-//}
+package fr.alexdoru.megawallsenhancementsmod.mixin.mixins;
+
+import fr.alexdoru.megawallsenhancementsmod.utils.NameUtil;
+import net.minecraft.scoreboard.ScorePlayerTeam;
+import net.minecraft.scoreboard.Scoreboard;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Slice;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Scoreboard.class)
+public class ScoreboardMixin {
+
+    @Inject(
+            method = "addPlayerToTeam",
+            slice = @Slice(from = @At(value = "INVOKE", target = "Lnet/minecraft/scoreboard/ScorePlayerTeam;getMembershipCollection()Ljava/util/Collection;")),
+            at = @At(value = "RETURN")
+    )
+    public void addPlayerToTeam(String player, String newTeam, CallbackInfoReturnable<Boolean> ci) {
+        if (ci.getReturnValueZ()) NameUtil.transformNameTablist(player);
+    }
+
+    @Inject(
+            method = "removePlayerFromTeam",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Ljava/util/Collection;remove(Ljava/lang/Object;)Z",
+                    shift = At.Shift.AFTER
+            ))
+    public void removePlayerFromTeam(String p_96512_1_, ScorePlayerTeam p_96512_2_, CallbackInfo ci) {
+        NameUtil.transformNameTablist(p_96512_1_);
+    }
+
+}
