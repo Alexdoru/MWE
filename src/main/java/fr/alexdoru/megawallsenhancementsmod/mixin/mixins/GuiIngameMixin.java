d9d38e4e7c1152b34db2d00eaa41ee41b1cc6e6f
Alexdoru <alex.dolley@free.fr>
2022-01-05 21:55:14 +0100

    changed local variable named as a result of changing to stable22 mappings
    (cherry picked from commit f93f0be2940d39c1a1129d05c543bb5c46086504)

--- src/main/java/fr/alexdoru/megawallsenhancementsmod/mixin/mixins/GuiIngameMixin.java
+++ src/main/java/fr/alexdoru/megawallsenhancementsmod/mixin/mixins/GuiIngameMixin.java
@@ -11,8 +11,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class GuiIngameMixin extends Gui {
 
     @Inject(method = "displayTitle", at = @At("HEAD"), cancellable = true)
-    public void displayTitle(String p_175178_1_, String p_175178_2_, int p_175178_3_, int p_175178_4_, int p_175178_5_, CallbackInfo ci) {
-        if (p_175178_2_ != null && p_175178_2_.contains("Get to the middle to stop the hunger!")) {
+    public void displayTitle(String title, String subTitle, int timeFadeIn, int displayTime, int timeFadeOut, CallbackInfo ci) {
+        if (subTitle != null && subTitle.contains("Get to the middle to stop the hunger!")) {
             ci.cancel();
         }
     }
