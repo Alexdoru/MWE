0e3634c3b850b03450995b0061723479240be1a8
Alexdoru <57050655+alexdoru@users.noreply.github.com>
2022-01-18 01:43:13 +0100

    added playerFinalkills field, initialized the field in the constructor and simplified the previous ASM to change the displayName
    (cherry picked from commit 205614ee99bfce18fa2dfc8b41bf43de39f7704c)

--- src/main/java/fr/alexdoru/megawallsenhancementsmod/asm/transformers/NetworkPlayerInfoTransformer.java
+++ src/main/java/fr/alexdoru/megawallsenhancementsmod/asm/transformers/NetworkPlayerInfoTransformer.java
@@ -1,7 +1,8 @@
 package fr.alexdoru.megawallsenhancementsmod.asm.transformers;
 
-import fr.alexdoru.megawallsenhancementsmod.asm.IMyClassTransformer;
 import fr.alexdoru.megawallsenhancementsmod.asm.ASMLoadingPlugin;
+import fr.alexdoru.megawallsenhancementsmod.asm.IMyClassTransformer;
+import org.objectweb.asm.FieldVisitor;
 import org.objectweb.asm.tree.*;
 
 import static org.objectweb.asm.Opcodes.*;
@@ -15,24 +16,38 @@ public class NetworkPlayerInfoTransformer implements IMyClassTransformer {
 
     @Override
     public ClassNode transform(ClassNode classNode) {
+
+        {
+            FieldVisitor fieldVisitor = classNode.visitField(ACC_PRIVATE, "playerFinalkills", "I", null, 0);
+            fieldVisitor.visitEnd();
+        }
+
         for (MethodNode methodNode : classNode.methods) {
             if (methodNode.name.equals("<init>") && methodNode.desc.equals(ASMLoadingPlugin.isObf ? "(Lgz$b;)V" : "(Lnet/minecraft/network/play/server/S38PacketPlayerListItem$AddPlayerData;)V")) {
                 for (AbstractInsnNode insnNode : methodNode.instructions.toArray()) {
                     if (insnNode.getOpcode() == PUTFIELD && ((FieldInsnNode) insnNode).owner.equals(ASMLoadingPlugin.isObf ? "bdc" : "net/minecraft/client/network/NetworkPlayerInfo") && ((FieldInsnNode) insnNode).name.equals(ASMLoadingPlugin.isObf ? "h" : "displayName")) {
+
+                        /*Replace line 48 with : this.displayname = NetworkPlayerInfoHook.getDisplayName(this.displayname, this.gameProfile)*/
+                        InsnList list = new InsnList();
+                        list.add(new VarInsnNode(ALOAD, 0));
+                        list.add(new FieldInsnNode(GETFIELD, ASMLoadingPlugin.isObf ? "bdc" : "net/minecraft/client/network/NetworkPlayerInfo", ASMLoadingPlugin.isObf ? "a" : "gameProfile", "Lcom/mojang/authlib/GameProfile;"));
+                        list.add(new MethodInsnNode(INVOKESTATIC, "fr/alexdoru/megawallsenhancementsmod/asm/hooks/NetworkPlayerInfoHook", "getDisplayName", ASMLoadingPlugin.isObf ? "(Leu;Lcom/mojang/authlib/GameProfile;)Leu;" : "(Lnet/minecraft/util/IChatComponent;Lcom/mojang/authlib/GameProfile;)Lnet/minecraft/util/IChatComponent;", false));
+                        methodNode.instructions.insertBefore(insnNode, list);
+
+                        /*Adds after line 48 : this.playersFinalKills = NetworkPlayerInfoHook.getPlayersFinals(this.gameProfile.getName())*/
                         AbstractInsnNode nextNode = insnNode.getNext();
                         if (nextNode != null) {
-                            InsnList list = new InsnList();
-                            list.add(new VarInsnNode(ALOAD, 0));
-                            list.add(new VarInsnNode(ALOAD, 0));
-                            list.add(new FieldInsnNode(GETFIELD, ASMLoadingPlugin.isObf ? "bdc" : "net/minecraft/client/network/NetworkPlayerInfo", ASMLoadingPlugin.isObf ? "h" : "displayName", ASMLoadingPlugin.isObf ? "Leu;" : "Lnet/minecraft/util/IChatComponent;"));
-                            list.add(new VarInsnNode(ALOAD, 0));
-                            list.add(new FieldInsnNode(GETFIELD, ASMLoadingPlugin.isObf ? "bdc" : "net/minecraft/client/network/NetworkPlayerInfo", ASMLoadingPlugin.isObf ? "a" : "gameProfile", "Lcom/mojang/authlib/GameProfile;"));
-                            list.add(new MethodInsnNode(INVOKESTATIC, "fr/alexdoru/megawallsenhancementsmod/asm/hooks/NetworkPlayerInfoHook", "getDisplayName", ASMLoadingPlugin.isObf ? "(Leu;Lcom/mojang/authlib/GameProfile;)Leu;" : "(Lnet/minecraft/util/IChatComponent;Lcom/mojang/authlib/GameProfile;)Lnet/minecraft/util/IChatComponent;", false));
-                            list.add(new FieldInsnNode(PUTFIELD, ASMLoadingPlugin.isObf ? "bdc" : "net/minecraft/client/network/NetworkPlayerInfo", ASMLoadingPlugin.isObf ? "h" : "displayName", ASMLoadingPlugin.isObf ? "Leu;" : "Lnet/minecraft/util/IChatComponent;"));
-                            methodNode.instructions.insertBefore(nextNode, list);
-                            ASMLoadingPlugin.logger.info("Transformed NetworkPlayerInfo");
-                            return classNode;
+                            InsnList list2 = new InsnList();
+                            list2.add(new VarInsnNode(ALOAD, 0));
+                            list2.add(new VarInsnNode(ALOAD, 0));
+                            list2.add(new FieldInsnNode(GETFIELD, ASMLoadingPlugin.isObf ? "bdc" : "net/minecraft/client/network/NetworkPlayerInfo", ASMLoadingPlugin.isObf ? "a" : "gameProfile", "Lcom/mojang/authlib/GameProfile;"));
+                            list2.add(new MethodInsnNode(INVOKEVIRTUAL, "com/mojang/authlib/GameProfile", "getName", "()Ljava/lang/String;", false));
+                            list2.add(new MethodInsnNode(INVOKESTATIC, "fr/alexdoru/megawallsenhancementsmod/asm/hooks/NetworkPlayerInfoHook", "getPlayersFinals", "(Ljava/lang/String;)I", false));
+                            list2.add(new FieldInsnNode(PUTFIELD, ASMLoadingPlugin.isObf ? "bdc" : "net/minecraft/client/network/NetworkPlayerInfo", "playerFinalkills", "I"));
+                            methodNode.instructions.insertBefore(nextNode, list2);
                         }
+                        ASMLoadingPlugin.logger.info("Transformed NetworkPlayerInfo");
+                        return classNode;
                     }
                 }
             }
