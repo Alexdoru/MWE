aeb6e89265b2b51cc8fda5dacc8ecd14c6c3964f
Alexdoru <57050655+alexdoru@users.noreply.github.com>
2022-02-03 04:31:59 +0100

    added timer protection to not drop sword accidentally
    (cherry picked from commit d3d7b3e6fcfafc24caacdaa8c018780676737aa8)

--- src/main/java/fr/alexdoru/megawallsenhancementsmod/asm/transformers/MinecraftTransformer.java
+++ src/main/java/fr/alexdoru/megawallsenhancementsmod/asm/transformers/MinecraftTransformer.java
@@ -4,8 +4,7 @@ import fr.alexdoru.megawallsenhancementsmod.asm.ASMLoadingPlugin;
 import fr.alexdoru.megawallsenhancementsmod.asm.IMyClassTransformer;
 import org.objectweb.asm.tree.*;
 
-import static org.objectweb.asm.Opcodes.INVOKESTATIC;
-import static org.objectweb.asm.Opcodes.INVOKEVIRTUAL;
+import static org.objectweb.asm.Opcodes.*;
 
 public class MinecraftTransformer implements IMyClassTransformer {
     @Override
@@ -19,6 +18,29 @@ public class MinecraftTransformer implements IMyClassTransformer {
         for (MethodNode methodNode : classNode.methods) {
             if (methodNode.name.equals(ASMLoadingPlugin.isObf ? "s" : "runTick") && methodNode.desc.equals("()V")) {
                 for (AbstractInsnNode insnNode : methodNode.instructions.toArray()) {
+
+                    if (insnNode.getOpcode() == INVOKEVIRTUAL && insnNode instanceof MethodInsnNode
+                            && ((MethodInsnNode) insnNode).owner.equals(ASMLoadingPlugin.isObf ? "wm" : "net/minecraft/entity/player/InventoryPlayer")
+                            && ((MethodInsnNode) insnNode).name.equals(ASMLoadingPlugin.isObf ? "d" : "changeCurrentItem")
+                            && ((MethodInsnNode) insnNode).desc.equals("(I)V")) {
+                        /*
+                         * Inject before line 1869
+                         * MinecraftHook.updateCurrentSlot(this);
+                         */
+                        methodNode.instructions.insertBefore(insnNode, updateCurrentSlotInsnList());
+                    }
+
+                    if (insnNode.getOpcode() == PUTFIELD && insnNode instanceof FieldInsnNode
+                            && ((FieldInsnNode) insnNode).owner.equals(ASMLoadingPlugin.isObf ? "wm" : "net/minecraft/entity/player/InventoryPlayer")
+                            && ((FieldInsnNode) insnNode).name.equals(ASMLoadingPlugin.isObf ? "c" : "currentItem")
+                            && ((FieldInsnNode) insnNode).desc.equals("I")) {
+                        /*
+                         * Inject before line 2077
+                         * MinecraftHook.updateCurrentSlot(this);
+                         */
+                        methodNode.instructions.insertBefore(insnNode, updateCurrentSlotInsnList());
+                    }
+
                     if (insnNode.getOpcode() == INVOKEVIRTUAL && insnNode instanceof MethodInsnNode
                             && ((MethodInsnNode) insnNode).owner.equals(ASMLoadingPlugin.isObf ? "bew" : "net/minecraft/client/entity/EntityPlayerSP")
                             && ((MethodInsnNode) insnNode).name.equals(ASMLoadingPlugin.isObf ? "a" : "dropOneItem")
@@ -37,10 +59,18 @@ public class MinecraftTransformer implements IMyClassTransformer {
                         methodNode.instructions.remove(insnNode);
                         ASMLoadingPlugin.logger.info("Transformed Minecraft");
                     }
+
                 }
             }
         }
 
         return classNode;
+    }
+
+    private InsnList updateCurrentSlotInsnList() {
+        InsnList list = new InsnList();
+        list.add(new VarInsnNode(ALOAD, 0));
+        list.add(new MethodInsnNode(INVOKESTATIC, "fr/alexdoru/megawallsenhancementsmod/asm/hooks/MinecraftHook", "updateCurrentSlot", ASMLoadingPlugin.isObf ? "(Lave;)V" : "(Lnet/minecraft/client/Minecraft;)V", false));
+        return list;
     }
 }
