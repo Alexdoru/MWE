f90894c74e15be3ee3dde7c16a3590a0b5218841
Alexdoru <alex.dolley@free.fr>
2022-02-03 04:31:59 +0100

    added timer protection to not drop sword accidentally
    (cherry picked from commit d3d7b3e6fcfafc24caacdaa8c018780676737aa8)

--- src/main/java/fr/alexdoru/megawallsenhancementsmod/asm/hooks/MinecraftHook.java
+++ src/main/java/fr/alexdoru/megawallsenhancementsmod/asm/hooks/MinecraftHook.java
@@ -1,15 +1,29 @@
 package fr.alexdoru.megawallsenhancementsmod.asm.hooks;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.item.ItemSword;
 
 public class MinecraftHook {
 
+    private static long lastSlotChangeFromSwordSlot;
+
     public static void dropOneItem(EntityPlayerSP thePlayer) {
-        if (thePlayer.getCurrentEquippedItem() == null || !(thePlayer.getCurrentEquippedItem().getItem() instanceof ItemSword)) {
-            thePlayer.dropOneItem(GuiScreen.isCtrlKeyDown());
+        if (System.currentTimeMillis() < lastSlotChangeFromSwordSlot + 100 || checkIfHoldingSword(thePlayer)) {
+            return;
         }
+        thePlayer.dropOneItem(GuiScreen.isCtrlKeyDown());
+    }
+
+    public static void updateCurrentSlot(Minecraft mc) {
+        if (checkIfHoldingSword(mc.thePlayer)) {
+            lastSlotChangeFromSwordSlot = System.currentTimeMillis();
+        }
+    }
+
+    private static boolean checkIfHoldingSword(EntityPlayerSP thePlayer) {
+        return thePlayer.getCurrentEquippedItem() != null && thePlayer.getCurrentEquippedItem().getItem() instanceof ItemSword;
     }
 
 }
