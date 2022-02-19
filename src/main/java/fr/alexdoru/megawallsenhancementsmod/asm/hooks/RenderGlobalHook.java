1a7aff4299a4fb0c9e46010ec4ad737665dc6431
Alexdoru <alex.dolley@free.fr>
2022-02-19 16:39:42 +0100

    added whitelist to still render certain items, golden apple, diamond armor etc
    (cherry picked from commit bd0e8ff387b010cd4315b4ac59b01329280df58c)

--- src/main/java/fr/alexdoru/megawallsenhancementsmod/asm/hooks/RenderGlobalHook.java
+++ src/main/java/fr/alexdoru/megawallsenhancementsmod/asm/hooks/RenderGlobalHook.java
@@ -4,34 +4,59 @@ import fr.alexdoru.megawallsenhancementsmod.config.ConfigHandler;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityItem;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+
+import java.util.HashSet;
 
 public class RenderGlobalHook {
 
     private static int[] entityItemCount = new int[256];
     private static int renderDistance = 256;
     private static int prevRenderDistance = renderDistance;
+    private static final HashSet<Item> itemWhitelist = new HashSet<>();
+
+    static {
+        itemWhitelist.add(Items.potionitem);
+        itemWhitelist.add(Items.diamond_sword);
+        itemWhitelist.add(Items.diamond_helmet);
+        itemWhitelist.add(Items.diamond_chestplate);
+        itemWhitelist.add(Items.diamond_leggings);
+        itemWhitelist.add(Items.diamond_boots);
+        itemWhitelist.add(Items.milk_bucket);
+        itemWhitelist.add(Items.golden_apple);
+    }
 
     public static void resetEntityItemCount() {
-        int entityCount = 0;
-        for (int i = 0; i < entityItemCount.length; i++) {
-            entityCount += entityItemCount[i];
-            if (entityCount > ConfigHandler.maxDroppedEntityRendered) {
-                renderDistance = i == 0 ? 1 : i;
-                prevRenderDistance = renderDistance;
-                entityItemCount = new int[256];
-                return;
+        if (ConfigHandler.limitDroppedEntityRendered) {
+            int entityCount = 0;
+            for (int i = 0; i < entityItemCount.length; i++) {
+                entityCount += entityItemCount[i];
+                if (entityCount > ConfigHandler.maxDroppedEntityRendered) {
+                    renderDistance = i == 0 ? 1 : i;
+                    prevRenderDistance = renderDistance;
+                    entityItemCount = new int[256];
+                    return;
+                }
             }
+            renderDistance = 256;
+            prevRenderDistance = renderDistance;
+            entityItemCount = new int[256];
         }
-        renderDistance = 256;
-        prevRenderDistance = renderDistance;
-        entityItemCount = new int[256];
     }
 
     public static void renderEntitySimple(RenderManager renderManagerIn, Entity entityIn, float partialTicks, double viewerX, double viewerY, double viewerZ) {
         if (ConfigHandler.limitDroppedEntityRendered && entityIn instanceof EntityItem) {
+            ItemStack itemstack = ((EntityItem) entityIn).getEntityItem();
+            Item item = itemstack.getItem();
+            if (itemWhitelist.contains(item)) {
+                entityItemCount[0]++;
+                renderManagerIn.renderEntitySimple(entityIn, partialTicks);
+                return;
+            }
             double d = Math.min(entityIn.getDistanceSq(viewerX, viewerY, viewerZ), 255.9999d);
-            int i = (int) d;
-            entityItemCount[i]++;
+            entityItemCount[(int) d]++;
             if (d <= prevRenderDistance) {
                 renderManagerIn.renderEntitySimple(entityIn, partialTicks);
             }
