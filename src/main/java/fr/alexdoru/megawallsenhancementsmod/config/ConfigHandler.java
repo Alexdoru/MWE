40158cb581405b022d7129c5829efc623655923f
Alexdoru <57050655+alexdoru@users.noreply.github.com>
2022-02-21 21:35:15 +0100

    changed value autoreport
    (cherry picked from commit 0c8495f289eb2b7cf67faca2df226225296153cd)

--- src/main/java/fr/alexdoru/megawallsenhancementsmod/config/ConfigHandler.java
+++ src/main/java/fr/alexdoru/megawallsenhancementsmod/config/ConfigHandler.java
@@ -148,7 +148,7 @@ public class ConfigHandler {
         Property pToggleicons = config.get(CATEGORY_NOCHEATERS, "Toggle Icons", true, "Display warning symbol on nametags of reported players");
         Property pTogglewarnings = config.get(CATEGORY_NOCHEATERS, "Toggle Warnings", false, "Gives warning messages in chat for reported players");
         Property pToggleautoreport = config.get(CATEGORY_NOCHEATERS, "Toggle Autoreport", false, "Automatically report previously reported players when they are in your lobby");
-        Property pTimeBetweenReports = config.get(CATEGORY_NOCHEATERS, "Time between reports", 12, "Time before the mod suggests to report the player again (hours)");
+        Property pTimeBetweenReports = config.get(CATEGORY_NOCHEATERS, "Time between reports", 4, "Time before the mod suggests to report the player again (hours)");
         Property pTimeAutoReport = config.get(CATEGORY_NOCHEATERS, "Time for autoreports", 14, "It won't autoreport players whose last report is older than this (days)");
         Property pdeleteReports = config.get(CATEGORY_NOCHEATERS, "Delete Old Report", false, "Deletes reports older than the specified value");
         Property ptimeDeleteReport = config.get(CATEGORY_NOCHEATERS, "Time delete reports", 365, "Reports older than this will be deleted on game start (days)");
