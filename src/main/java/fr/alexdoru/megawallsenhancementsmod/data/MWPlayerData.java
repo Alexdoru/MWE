fc0d6e911b35c45cbf3c6a8759365cc36f2ce5d1
Alexdoru <57050655+alexdoru@users.noreply.github.com>
2022-02-25 01:27:46 +0100

    removed todo
    (cherry picked from commit 305ffe31420129ca120c004ebd97f843ec036ee4)

--- src/main/java/fr/alexdoru/megawallsenhancementsmod/data/MWPlayerData.java
+++ src/main/java/fr/alexdoru/megawallsenhancementsmod/data/MWPlayerData.java
@@ -4,15 +4,12 @@ import fr.alexdoru.nocheatersmod.data.WDR;
 import net.minecraft.util.IChatComponent;
 
 public class MWPlayerData {
-    // TODO faut update les fields quand on tape un nouveau WDR, update un WDR ou enleve un WDR, pareil pour scangame, squadname etc dans move player de la stalklist
+
     public WDR wdr;
     public IChatComponent extraPrefix;
     public String squadname;
     public IChatComponent displayName;
     public int playerFinalkills;
-    //public boolean isNicked;
-    // prestrige 5
-    // suffix
 
     public MWPlayerData(WDR wdr, IChatComponent extraPrefix, String squadname, IChatComponent displayNameIn, int playerFinalkills) {
         this.wdr = wdr;
