1d1b3de187365db0525dfebbfd9a5033c81ecb99
Alexdoru <alex.dolley@free.fr>
2022-02-27 21:40:01 +0100

    removed item filter for dropped items
    (cherry picked from commit da3af77eba8dd3fcc564d90ebcac9f9c92544399)

--- src/main/java/fr/alexdoru/megawallsenhancementsmod/asm/hooks/RenderGlobalHook.java
+++ src/main/java/fr/alexdoru/megawallsenhancementsmod/asm/hooks/RenderGlobalHook.java
@@ -4,29 +4,12 @@ import fr.alexdoru.megawallsenhancementsmod.config.ConfigHandler;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityItem;
-import net.minecraft.init.Items;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-
-import java.util.HashSet;
 
 public class RenderGlobalHook {
 
     private static int[] entityItemCount = new int[256];
     private static int renderDistance = 256;
     private static int prevRenderDistance = renderDistance;
-    private static final HashSet<Item> itemWhitelist = new HashSet<>();
-
-    static {
-        itemWhitelist.add(Items.potionitem);
-        itemWhitelist.add(Items.diamond_sword);
-        itemWhitelist.add(Items.diamond_helmet);
-        itemWhitelist.add(Items.diamond_chestplate);
-        itemWhitelist.add(Items.diamond_leggings);
-        itemWhitelist.add(Items.diamond_boots);
-        itemWhitelist.add(Items.milk_bucket);
-        itemWhitelist.add(Items.golden_apple);
-    }
 
     public static void resetEntityItemCount() {
         if (ConfigHandler.limitDroppedEntityRendered) {
@@ -48,13 +31,6 @@ public class RenderGlobalHook {
 
     public static void renderEntitySimple(RenderManager renderManagerIn, Entity entityIn, float partialTicks, double viewerX, double viewerY, double viewerZ) {
         if (ConfigHandler.limitDroppedEntityRendered && entityIn instanceof EntityItem) {
-            ItemStack itemstack = ((EntityItem) entityIn).getEntityItem();
-            Item item = itemstack.getItem();
-            if (itemWhitelist.contains(item)) {
-                entityItemCount[0]++;
-                renderManagerIn.renderEntitySimple(entityIn, partialTicks);
-                return;
-            }
             double d = Math.min(entityIn.getDistanceSq(viewerX, viewerY, viewerZ), 255.9999d);
             entityItemCount[(int) d]++;
             if (d <= prevRenderDistance) {
