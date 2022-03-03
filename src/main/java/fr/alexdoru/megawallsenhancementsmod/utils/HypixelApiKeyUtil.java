4335c93f7a589ec45a856f006910f7637d7abcf7
Alexdoru <alex.dolley@free.fr>
2022-03-03 18:13:50 +0100

    delete commented out code in HypixelApiKeyUtil
    (cherry picked from commit a6c870a346d4c5d7bcbd9b61513eea9da82c392b)

--- src/main/java/fr/alexdoru/megawallsenhancementsmod/utils/HypixelApiKeyUtil.java
+++ src/main/java/fr/alexdoru/megawallsenhancementsmod/utils/HypixelApiKeyUtil.java
@@ -9,19 +9,7 @@ import static fr.alexdoru.megawallsenhancementsmod.utils.ChatUtil.getTagMW;
 
 public class HypixelApiKeyUtil {
 
-//	private static List<Long> request_times = new ArrayList<Long>();
-//	public static final int NB_MAX_REQUESTS = 120;
-
     public static String getApiKey() {
-
-//		long time = System.currentTimeMillis();
-//		request_times.add(time);
-//		request_times.removeIf(o -> (o.longValue() + 60000L < time));
-//
-//		if(request_times.size() > NB_MAX_REQUESTS) {
-//			throw new ApiException("Exceeding the limit of " + NB_MAX_REQUESTS + " requests per minute allowed by Hypixel");
-//		}
-
         return ConfigHandler.APIKey;
     }
 
@@ -34,14 +22,5 @@ public class HypixelApiKeyUtil {
     public static boolean apiKeyIsNotSetup() {
         return ConfigHandler.APIKey == null || ConfigHandler.APIKey.equals("");
     }
-
-//	/*
-//	 * returns the amounts of requests send in the last minute
-//	 */
-//	public static int getRequestAmount() {
-//		long time = System.currentTimeMillis();
-//		request_times.removeIf(o -> (o.longValue() + 60000L < time));
-//		return request_times.size();	
-//	}
 
 }
