5b1f6082ce8859daeb601789d4ee3172d80ba7d6
Alexdoru <57050655+alexdoru@users.noreply.github.com>
2022-03-14 16:41:25 +0100

    delete nocheaters reportworld feature
    (cherry picked from commit 873dd6c3a73787d383098716aa9601624411e0ae)

--- src/main/java/fr/alexdoru/nocheatersmod/commands/CommandNocheaters.java
+++ src/main/java/fr/alexdoru/nocheatersmod/commands/CommandNocheaters.java
@@ -4,8 +4,6 @@ import fr.alexdoru.fkcountermod.utils.DelayedTask;
 import fr.alexdoru.megawallsenhancementsmod.api.exceptions.ApiException;
 import fr.alexdoru.megawallsenhancementsmod.api.hypixelplayerdataparser.LoginData;
 import fr.alexdoru.megawallsenhancementsmod.api.requests.HypixelPlayerData;
-import fr.alexdoru.megawallsenhancementsmod.asm.accessor.GameProfileAccessor;
-import fr.alexdoru.megawallsenhancementsmod.data.MWPlayerData;
 import fr.alexdoru.megawallsenhancementsmod.gui.NoCheatersConfigGuiScreen;
 import fr.alexdoru.megawallsenhancementsmod.utils.ChatUtil;
 import fr.alexdoru.megawallsenhancementsmod.utils.DateUtil;
@@ -15,7 +13,6 @@ import fr.alexdoru.nocheatersmod.data.WDR;
 import fr.alexdoru.nocheatersmod.data.WdredPlayers;
 import fr.alexdoru.nocheatersmod.events.NoCheatersEvents;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.network.NetworkPlayerInfo;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.command.NumberInvalidException;
@@ -52,27 +49,17 @@ public class CommandNocheaters extends CommandBase {
             if (list.isEmpty()) {
                 addChatMessage(new ChatComponentText(getTagNoCheaters() + EnumChatFormatting.GREEN + "No reported player here !"));
             } else {
-                addChatMessage(new ChatComponentText(getTagNoCheaters() +
-                        EnumChatFormatting.YELLOW + "Reported players : ")
-                        .appendSibling(new ChatComponentText(EnumChatFormatting.DARK_GREEN + "Report ALL"))
-                        .setChatStyle(new ChatStyle()
-                                .setChatHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ChatComponentText(EnumChatFormatting.YELLOW + "Click to report again all the reported players in your world")))
-                                .setChatClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/nocheaters reportworld"))));
+                addChatMessage(new ChatComponentText(getTagNoCheaters() + EnumChatFormatting.YELLOW + "Reported players : "));
                 for (IChatComponent report : list) {
                     addChatMessage(report);
                 }
-
             }
 
             return;
 
         }
 
-        if (args.length == 1 && args[0].equalsIgnoreCase("reportworld")) {
-
-            reportWorld();
-
-        } else if (args.length == 1 && args[0].equalsIgnoreCase("config")) {
+        if (args.length == 1 && args[0].equalsIgnoreCase("config")) {
 
             new DelayedTask(() -> mc.displayGuiScreen(new NoCheatersConfigGuiScreen()), 1);
 
@@ -177,41 +164,13 @@ public class CommandNocheaters extends CommandBase {
 
     @Override
     public List<String> addTabCompletionOptions(ICommandSender sender, String[] args, BlockPos pos) {
-        String[] arguments = {"config", "reportlist", "reportworld", "stalkreportlist"};
+        String[] arguments = {"config", "reportlist", "stalkreportlist"};
         return getListOfStringsMatchingLastWord(args, arguments);
     }
 
     @Override
     public boolean canCommandSenderUseCommand(ICommandSender sender) {
         return true;
-    }
-
-    /**
-     * This gets called after you typed /nocheaters if you click on the button Report World
-     */
-    private void reportWorld() {
-        long datenow = (new Date()).getTime();
-        int nbreport = 0;
-        for (NetworkPlayerInfo networkPlayerInfo : mc.getNetHandler().getPlayerInfoMap()) {
-            String playerName = networkPlayerInfo.getGameProfile().getName();
-            MWPlayerData mwPlayerData = ((GameProfileAccessor) networkPlayerInfo.getGameProfile()).getMWPlayerData();
-            if (mwPlayerData == null) {
-                continue;
-            }
-            WDR wdr = mwPlayerData.wdr;
-            if (wdr == null) {
-                continue;
-            }
-            if (wdr.canBeReported(datenow)) {
-                wdr.timestamp = datenow;
-                new DelayedTask(() -> {
-                    if (mc.thePlayer != null) {
-                        mc.thePlayer.sendChatMessage("/wdr " + playerName + " cheating");
-                    }
-                }, 30 * nbreport);
-                nbreport++;
-            }
-        }
     }
 
     /**
