12a1528749982f2d7346a6773e47bce0cd5ad689
Alexdoru <57050655+alexdoru@users.noreply.github.com>
2022-03-20 02:01:45 +0100

    changed ban detection message
    (cherry picked from commit 417c2f7f32a2fcdb22fb6d145c5a19245f61e9e9)

--- src/main/java/fr/alexdoru/megawallsenhancementsmod/events/ChatEvents.java
+++ src/main/java/fr/alexdoru/megawallsenhancementsmod/events/ChatEvents.java
@@ -229,14 +229,9 @@ public class ChatEvents {
                 new DelayedTask(() -> {
                     String recentlyDisconnectedPlayers = NetHandlerPlayClientHook.getRecentlyDisconnectedPlayers();
                     if (!recentlyDisconnectedPlayers.equals("")) {
-                        ChatUtil.addChatMessage(new ChatComponentText(
-                                ChatUtil.getTagMW() + EnumChatFormatting.RED + "Player(s) recently disconnected : " + EnumChatFormatting.YELLOW + recentlyDisconnectedPlayers)
-                                .setChatStyle(new ChatStyle()
-                                        .setChatHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ChatComponentText(
-                                                EnumChatFormatting.GREEN + "Those are the players disconnected in the last 2 seconds, click this message to run : \n\n"
-                                                        + EnumChatFormatting.YELLOW + "/stalk" + recentlyDisconnectedPlayers)))
-                                        .setChatClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/stalk" + recentlyDisconnectedPlayers)))
-                        );
+                        ChatUtil.addChatMessage(new ChatComponentText(ChatUtil.getTagNoCheaters() + EnumChatFormatting.RED + "Player(s) recently disconnected :\n" + ChatUtil.getTagNoCheaters() + EnumChatFormatting.AQUA + recentlyDisconnectedPlayers).setChatStyle(new ChatStyle()
+                                .setChatHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ChatComponentText(EnumChatFormatting.GREEN + "Those are the players disconnected in the last 2 seconds, click this message to run : \n\n" + EnumChatFormatting.YELLOW + "/stalk" + recentlyDisconnectedPlayers)))
+                                .setChatClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/stalk" + recentlyDisconnectedPlayers))));
                     }
                 }, 20);
                 //return;
