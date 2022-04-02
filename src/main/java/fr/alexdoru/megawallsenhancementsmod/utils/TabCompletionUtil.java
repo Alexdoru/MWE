1209c0a2b7074e18798b14bb111113f1e85fa6bb
Alexdoru <57050655+alexdoru@users.noreply.github.com>
2022-04-02 19:05:56 +0200

    added null check in TabCompletionUtil
    (cherry picked from commit db46eec003ccbf337ecab9c9119f9f1f2bf4a3bc)

--- src/main/java/fr/alexdoru/megawallsenhancementsmod/utils/TabCompletionUtil.java
+++ src/main/java/fr/alexdoru/megawallsenhancementsmod/utils/TabCompletionUtil.java
@@ -2,31 +2,28 @@ package fr.alexdoru.megawallsenhancementsmod.utils;
 
 import fr.alexdoru.fkcountermod.FKCounterMod;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.network.NetHandlerPlayClient;
 import net.minecraft.client.network.NetworkPlayerInfo;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 public class TabCompletionUtil {
 
     public static List<String> getOnlinePlayersByName() {
-
-        ArrayList<String> players = new ArrayList<>();
-
+        List<String> players = new ArrayList<>();
         if (FKCounterMod.isitPrepPhase) {
             return players;
         }
-
-        Collection<NetworkPlayerInfo> playerCollection = Minecraft.getMinecraft().getNetHandler().getPlayerInfoMap();
-
-        for (NetworkPlayerInfo networkPlayerInfo : playerCollection) {
-            String playerName = networkPlayerInfo.getGameProfile().getName();
-            if (playerName != null) {
-                players.add(playerName);
+        final NetHandlerPlayClient netHandler = Minecraft.getMinecraft().getNetHandler();
+        if (netHandler != null) {
+            for (NetworkPlayerInfo networkPlayerInfo : netHandler.getPlayerInfoMap()) {
+                String playerName = networkPlayerInfo.getGameProfile().getName();
+                if (playerName != null) {
+                    players.add(playerName);
+                }
             }
         }
-
         return players;
     }
 
