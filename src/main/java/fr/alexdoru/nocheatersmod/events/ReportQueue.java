a9e1f9baf2fd07d2816ed43471d0eb8c90816a9d
Alexdoru <alex.dolley@free.fr>
2022-04-03 02:13:36 +0200

    add the reports sent from the report queue to the check for spam
    (cherry picked from commit 2caa3fb42abab9d1571a6256cd15685f1ec0ffec)

--- src/main/java/fr/alexdoru/nocheatersmod/events/ReportQueue.java
+++ src/main/java/fr/alexdoru/nocheatersmod/events/ReportQueue.java
@@ -20,6 +20,7 @@ public class ReportQueue {
     private final Minecraft mc = Minecraft.getMinecraft();
     private int counter;
     private final List<ReportInQueue> queueList = new ArrayList<>();
+    private static final List<Long> commandUsageTimeList = new ArrayList<>();
     private static final Random random = new Random();
 
     @SubscribeEvent
@@ -33,6 +34,7 @@ public class ReportQueue {
             String playername = queueList.remove(queueList.size() - 1).reportedPlayer;
             mc.thePlayer.sendChatMessage("/wdr " + playername + " cheating");
             counter = (int) (TIME_BETWEEN_REPORTS + (10d * random.nextGaussian() / 6d));
+            addReportTimestamp();
         }
 
         if (isReportQueueInactive()) {
@@ -109,6 +111,17 @@ public class ReportQueue {
         return msg.toString();
     }
 
+    /**
+     * Called everytime a report is sent to check that the player doesn't send to many reports in a short time
+     */
+    public void addReportTimestamp() {
+        long l = System.currentTimeMillis();
+        commandUsageTimeList.removeIf(o -> (o + 2 * 60 * 1000L < l));
+        if (commandUsageTimeList.size() >= 5) {
+            ChatUtil.addChatMessage(new ChatComponentText(ChatUtil.getTagNoCheaters() + EnumChatFormatting.RED + "Don't report too many players at once or Hypixel will ignore your reports thinking you are a bot trying to flood their system"));
+        }
+        commandUsageTimeList.add(l);
+    }
 }
 
 class ReportInQueue {
