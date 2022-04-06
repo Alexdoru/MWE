17dcebc2b94e45056b4397ff3f89c32f3bf4a806
Alexdoru <57050655+alexdoru@users.noreply.github.com>
2022-04-06 23:02:09 +0200

    ----------------REALEASE 1.7

--- src/main/java/fr/alexdoru/nocheatersmod/NoCheatersMod.java
+++ src/main/java/fr/alexdoru/nocheatersmod/NoCheatersMod.java
@@ -16,7 +16,7 @@ import java.io.File;
 public class NoCheatersMod {
 
     public static final String modid = "nocheaters";
-    public static final String version = "2.2";
+    public static final String version = "2.3";
 
     public static final Logger logger = LogManager.getLogger("NoCheaters");
     public static final KeyBinding addtimemark_key = new KeyBinding("Add Timestamp", 0, "NoCheaters");
