681e570f448b94e86dec37f57226239eb32e0c10
Alexdoru <57050655+alexdoru@users.noreply.github.com>
2022-05-28 02:29:41 +0200

    auto updater

--- src/main/java/fr/alexdoru/megawallsenhancementsmod/updater/ModUpdater.java
+++ src/main/java/fr/alexdoru/megawallsenhancementsmod/updater/ModUpdater.java
@@ -1,11 +1,13 @@
 package fr.alexdoru.megawallsenhancementsmod.updater;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import fr.alexdoru.megawallsenhancementsmod.MegaWallsEnhancementsMod;
 import fr.alexdoru.megawallsenhancementsmod.api.HttpClient;
 import fr.alexdoru.megawallsenhancementsmod.api.exceptions.ApiException;
+import fr.alexdoru.megawallsenhancementsmod.config.ConfigHandler;
 import fr.alexdoru.megawallsenhancementsmod.utils.ChatUtil;
 import fr.alexdoru.megawallsenhancementsmod.utils.JsonUtil;
 import fr.alexdoru.megawallsenhancementsmod.utils.Multithreading;
@@ -15,17 +17,22 @@ import net.minecraft.event.HoverEvent;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.ChatStyle;
 import net.minecraft.util.EnumChatFormatting;
+import net.minecraft.util.Util;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 
+import java.io.*;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+
 public class ModUpdater {
 
-    private static final String GITHUB_API_URL = "https://api.github.com/repos/Alexdoru/MegaWallsEnhancements/releases";
-    private static final String GITHUB_URL = "https://github.com/Alexdoru/MegaWallsEnhancements/releases";
+    private static final Minecraft mc = Minecraft.getMinecraft();
+    private static boolean hasTriggered = false;
     public static boolean isUpTodate = false;
-    private final Minecraft mc = Minecraft.getMinecraft();
-    private boolean hasTriggered = false;
 
     @SubscribeEvent
     public void onTick(TickEvent.ClientTickEvent event) {
@@ -43,8 +50,12 @@ public class ModUpdater {
         }
     }
 
-    public static void checkForUpdate() throws ApiException {
+    /**
+     * https://github.com/DeDiamondPro/Auto-Updater
+     */
+    public static void checkForUpdate() throws ApiException, IOException {
 
+        String GITHUB_API_URL = "https://api.github.com/repos/Alexdoru/MegaWallsEnhancements/releases";
         HttpClient httpClient = new HttpClient(GITHUB_API_URL);
         String rawresponse = httpClient.getrawresponse();
         if (rawresponse == null) {
@@ -62,6 +73,7 @@ public class ModUpdater {
 
         int latestVersion = 0;
         String version = "";
+        String browser_download_url = null;
 
         for (JsonElement jsonElement : element.getAsJsonArray()) {
             JsonObject release = jsonElement.getAsJsonObject();
@@ -71,20 +83,106 @@ public class ModUpdater {
                 if (releaseVersion > latestVersion) {
                     latestVersion = releaseVersion;
                     version = tag_name;
+                    final JsonElement assets = release.get("assets");
+                    if (assets != null && assets.isJsonArray()) {
+                        final JsonArray assetsJsonArray = assets.getAsJsonArray();
+                        for (JsonElement assetsElement : assetsJsonArray) {
+                            if (assetsElement != null && assetsElement.isJsonObject()) {
+                                final JsonObject assetsJsonObject = assetsElement.getAsJsonObject();
+                                browser_download_url = JsonUtil.getString(assetsJsonObject, "browser_download_url");
+                            }
+                        }
+                    }
                 }
             }
         }
 
         if (Integer.parseInt(MegaWallsEnhancementsMod.version.replace(".", "")) < latestVersion) {
+
+            String GITHUB_URL = "https://github.com/Alexdoru/MegaWallsEnhancements/releases";
             ChatUtil.addChatMessage(new ChatComponentText(ChatUtil.getTagMW() + EnumChatFormatting.RED + EnumChatFormatting.BOLD + "Mega Walls Enhancements "
                     + EnumChatFormatting.GOLD + "version v" + version + EnumChatFormatting.GREEN + " is available, click this message to see the changelog and download page.")
                     .setChatStyle(new ChatStyle()
                             .setChatClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, GITHUB_URL))
                             .setChatHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ChatComponentText(EnumChatFormatting.YELLOW + GITHUB_URL)))));
+
+            if (ConfigHandler.automaticUpdate && browser_download_url != null && browser_download_url.endsWith(".jar")) {
+
+                File cacheDir = new File("config/updatecache");
+                if (!cacheDir.exists() && !cacheDir.mkdir()) {
+                    throw new IllegalStateException("Could not create cache folder");
+                }
+
+                String newModFileName = getFileName(browser_download_url);
+                File modCacheFile = new File(cacheDir, newModFileName);
+                downloadFileTo(browser_download_url, modCacheFile);
+                System.out.println("Downloaded MWEnhancement Update");
+
+                String GITHUB_DELETER_URL = "https://github.com/W-OVERFLOW/Deleter/releases/download/v1.4/Deleter-1.4.jar";// TODO essayer de faire marcher le 1.5
+                String deleterFileName = getFileName(GITHUB_DELETER_URL);
+                File deleterFile = new File(cacheDir, deleterFileName);
+                downloadFileTo(GITHUB_DELETER_URL, deleterFile);
+                System.out.println("Downloaded Mod Deleter");
+
+                if (modCacheFile.exists() && deleterFile.exists()) {
+
+                    ChatUtil.addChatMessage(new ChatComponentText(ChatUtil.getTagMW() + EnumChatFormatting.RED + EnumChatFormatting.BOLD + "Mega Walls Enhancements " + EnumChatFormatting.GOLD + "version v" + version + EnumChatFormatting.GREEN + " has been downloaded and will be installed automatically when closing your game."));
+
+                    Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                        try {
+                            final File oldJarFile = MegaWallsEnhancementsMod.jarFile;
+                            final File newJarFile = new File(oldJarFile.getParent(), newModFileName);
+                            if (newJarFile.createNewFile() && modCacheFile.exists() && oldJarFile.exists()) {
+                                try (InputStream source = new FileInputStream(modCacheFile); OutputStream dest = new FileOutputStream(newJarFile)) {
+                                    byte[] buffer = new byte[1024];
+                                    int length;
+                                    while ((length = source.read(buffer)) > 0) {
+                                        dest.write(buffer, 0, length);
+                                    }
+                                }
+                                modCacheFile.delete();
+                                deleteOldJar(oldJarFile.getAbsolutePath(), deleterFile);
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }));
+
+                }
+
+            }
+
         } else {
+
             isUpTodate = true;
+
         }
 
+    }
+
+    private static void downloadFileTo(String browser_download_url, File cacheFile) throws IOException {
+        URLConnection connection = new URL(browser_download_url).openConnection();
+        connection.setRequestProperty("User-Agent", "Updater");
+        InputStream inputStream = connection.getInputStream();
+        Files.copy(inputStream, cacheFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        inputStream.close();
+    }
+
+    private static String getFileName(String downloadUrl) {
+        return downloadUrl.substring(downloadUrl.lastIndexOf("/") + 1);
+    }
+
+    private static void deleteOldJar(String absolutePathToDelete, File deleter) throws IOException {
+        if (Util.getOSType() == Util.EnumOS.LINUX) {
+            Runtime.getRuntime().exec("chmod +x \"" + deleter.getAbsolutePath() + "\"");
+        } else if (Util.getOSType() == Util.EnumOS.OSX) {
+            Runtime.getRuntime().exec("chmod 755 \"" + deleter.getAbsolutePath() + "\"");
+        }
+        Runtime.getRuntime().exec(
+                "java -jar " + deleter.getName() + " \"" + absolutePathToDelete + "\"",// TODO tester de changer
+                null,
+                deleter.getParentFile()
+        );
     }
 
 }
