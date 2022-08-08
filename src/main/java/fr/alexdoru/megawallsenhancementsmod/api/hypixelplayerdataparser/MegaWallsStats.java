7a74a14101c4b6251df78b7d8830ada6c65d920d
Alexdoru <57050655+alexdoru@users.noreply.github.com>
2022-08-09 00:13:05 +0200

    stop initializing variables at 0
    (cherry picked from commit dec7e750ca8b94dab763cf26e7d3fc9a7e856496)

--- src/main/java/fr/alexdoru/megawallsenhancementsmod/api/hypixelplayerdataparser/MegaWallsStats.java
+++ src/main/java/fr/alexdoru/megawallsenhancementsmod/api/hypixelplayerdataparser/MegaWallsStats.java
@@ -23,35 +23,35 @@ public class MegaWallsStats {
     private final LinkedHashMap<String, Integer[]> classpointsMap = new LinkedHashMap<>();
     private String chosen_class;
     private String chosen_skin_class;
-    private int coins = 0;
-    private int mythic_favor = 0;
-    private int wither_damage = 0;
-    private int wither_kills = 0;
-    private int wins = 0;
-    private int wins_face_off = 0;
-    private int wins_practice = 0;
-    private int losses = 0;
-    private int kills = 0;
-    private int defender_kills = 0;
-    //private int assists = 0; // useless
-    private int deaths = 0;
-    private int final_kills = 0;
-    //private int final_kills_standard = 0; // useless
-    //private int final_assists = 0; // useless
-    //private int final_assists_standard = 0; // useless
-    private int final_deaths = 0;
-    private float kdr = 0;
-    private float fkdr = 0;
-    private float wlr = 0;
-    private float wither_damage_game = 0;
-    private float def_kill_game = 0;
-    private int time_played = 0;
-    private int nbprestiges = 0;
-    private int total_classpoints = 0;
-    private int legendary_skins = 0;
+    private int coins;
+    private int mythic_favor;
+    private int wither_damage;
+    private int wither_kills;
+    private int wins;
+    private int wins_face_off;
+    private int wins_practice;
+    private int losses;
+    private int kills;
+    private int defender_kills;
+    //private int assists; // useless
+    private int deaths;
+    private int final_kills;
+    //private int final_kills_standard; // useless
+    //private int final_assists; // useless
+    //private int final_assists_standard; // useless
+    private int final_deaths;
+    private float kdr;
+    private float fkdr;
+    private float wlr;
+    private float wither_damage_game;
+    private float def_kill_game;
+    private int time_played;
+    private int nbprestiges;
+    private int total_classpoints;
+    private int legendary_skins;
 
-    private int games_played = 0;
-    private float fkpergame = 0;
+    private int games_played;
+    private float fkpergame;
 
     private JsonObject classesdata = null;
 
