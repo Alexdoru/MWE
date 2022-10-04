bcfc14a2c205b2f495e99db1fcbca58fe5763093
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2022-10-04 18:44:36 +0200

    add getChatLines accessor
    (cherry picked from commit 3aaed8eff51417892e946be9250427ca579b4dce)

--- src/main/java/fr/alexdoru/megawallsenhancementsmod/asm/accessor/GuiNewChatAccessor.java
+++ src/main/java/fr/alexdoru/megawallsenhancementsmod/asm/accessor/GuiNewChatAccessor.java
@@ -1,6 +1,11 @@
 package fr.alexdoru.megawallsenhancementsmod.asm.accessor;
 
+import net.minecraft.client.gui.ChatLine;
+
+import java.util.List;
+
 public interface GuiNewChatAccessor {
     void deleteWarningMessagesFor(String playername);
     void deleteAllWarningMessages();
+    List<ChatLine> getChatLines();
 }
