0708f1b3d23d7e711eb2f21717a38b3f5700ac3d
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2022-10-07 11:21:13 +0200

    create WdredPlayers.init()
    (cherry picked from commit 62d6c5d9317701acb11bf649103be51f5a04266a)

--- src/main/java/fr/alexdoru/nocheatersmod/NoCheatersMod.java
+++ src/main/java/fr/alexdoru/nocheatersmod/NoCheatersMod.java
@@ -6,15 +6,12 @@ import fr.alexdoru.megawallsenhancementsmod.commands.CommandUnWDR;
 import fr.alexdoru.megawallsenhancementsmod.commands.CommandWDR;
 import fr.alexdoru.nocheatersmod.data.WdredPlayers;
 import fr.alexdoru.nocheatersmod.events.NoCheatersEvents;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraftforge.client.ClientCommandHandler;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.io.File;
 
 public class NoCheatersMod {
 
@@ -25,17 +22,13 @@ public class NoCheatersMod {
     public static final KeyBinding addtimemark_key = new KeyBinding("Add Timestamp", 0, "NoCheaters");
 
     public static void init() {
-
         ClientRegistry.registerKeyBinding(addtimemark_key);
         MinecraftForge.EVENT_BUS.register(new NoCheatersEvents());
         ClientCommandHandler.instance.registerCommand(new CommandWDR());
         ClientCommandHandler.instance.registerCommand(new CommandUnWDR());
         ClientCommandHandler.instance.registerCommand(new CommandReport());
         ClientCommandHandler.instance.registerCommand(new CommandNocheaters());
-        WdredPlayers.wdrsFile = new File(Minecraft.getMinecraft().mcDataDir, "config/wdred.txt");
-        Runtime.getRuntime().addShutdownHook(new Thread(WdredPlayers::saveReportedPlayers));
-        WdredPlayers.loadReportedPlayers();
-
+        WdredPlayers.init();
     }
 
 }
