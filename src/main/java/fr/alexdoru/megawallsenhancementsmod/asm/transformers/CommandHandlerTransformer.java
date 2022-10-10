b12dfeb0d94e14d45c3a373aae99afb5d822ff8e
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2022-10-10 16:57:00 +0200

    forgot to remove a debug message in CommandHandlerTransformer
    (cherry picked from commit 7e3c7f431be1743bfe24080fb2c62cd79ddc0614)

--- src/main/java/fr/alexdoru/megawallsenhancementsmod/asm/transformers/CommandHandlerTransformer.java
+++ src/main/java/fr/alexdoru/megawallsenhancementsmod/asm/transformers/CommandHandlerTransformer.java
@@ -19,7 +19,6 @@ public class CommandHandlerTransformer implements IMyClassTransformer {
         status.setInjectionPoints(1);
         for (final MethodNode methodNode : classNode.methods) {
             if (methodNode.name.equals(ASMLoadingPlugin.isObf ? "a" : "getTabCompletionOptions") && methodNode.desc.equals(ASMLoadingPlugin.isObf ? "(Lm;Ljava/lang/String;Lcj;)Ljava/util/List;" : "(Lnet/minecraft/command/ICommandSender;Ljava/lang/String;Lnet/minecraft/util/BlockPos;)Ljava/util/List;")) {
-                ASMLoadingPlugin.logger.info("found target method");
                 for (final AbstractInsnNode insnNode : methodNode.instructions.toArray()) {
                     if (insnNode instanceof InsnNode && insnNode.getOpcode() == ICONST_0) {
                         final AbstractInsnNode nextNode = insnNode.getNext();
