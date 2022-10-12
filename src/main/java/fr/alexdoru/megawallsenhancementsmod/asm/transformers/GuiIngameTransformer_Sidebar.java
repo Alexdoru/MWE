a79244d622b0a0bacd9303c10c10c1eeecc34975
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2022-10-12 20:51:51 +0200

    split GuiIngameTransformer
    (cherry picked from commit 608e543be18cf9f1e90af9f50837627881f390d8)

--- src/main/java/fr/alexdoru/megawallsenhancementsmod/asm/transformers/GuiIngameTransformer_Sidebar.java
+++ src/main/java/fr/alexdoru/megawallsenhancementsmod/asm/transformers/GuiIngameTransformer_Sidebar.java
@@ -1,6 +1,5 @@
 package fr.alexdoru.megawallsenhancementsmod.asm.transformers;
 
-import fr.alexdoru.megawallsenhancementsmod.asm.FieldMapping;
 import fr.alexdoru.megawallsenhancementsmod.asm.IMyClassTransformer;
 import fr.alexdoru.megawallsenhancementsmod.asm.InjectionStatus;
 import fr.alexdoru.megawallsenhancementsmod.asm.MethodMapping;
@@ -8,7 +7,7 @@ import org.objectweb.asm.tree.*;
 
 import static org.objectweb.asm.Opcodes.*;
 
-public class GuiIngameTransformer implements IMyClassTransformer {
+public class GuiIngameTransformer_Sidebar implements IMyClassTransformer {
 
     @Override
     public String getTargetClassName() {
@@ -17,19 +16,8 @@ public class GuiIngameTransformer implements IMyClassTransformer {
 
     @Override
     public ClassNode transform(ClassNode classNode, InjectionStatus status) {
-        status.setInjectionPoints(2);
+        status.setInjectionPoints(1);
         for (final MethodNode methodNode : classNode.methods) {
-
-            if (checkMethodNode(methodNode, MethodMapping.DISPLAYTITLE)) {
-                for (final AbstractInsnNode insnNode : methodNode.instructions.toArray()) {
-                    if (checkVarInsnNode(insnNode, ALOAD, 2) && checkFieldInsnNode(insnNode.getNext(), PUTFIELD, FieldMapping.GUIINGAME$DISPLAYEDSUBTITLE)) {
-                        methodNode.instructions.insert(insnNode, new MethodInsnNode(INVOKESTATIC, getHookClass("GuiIngameHook"), "cancelHungerTitle", "(Ljava/lang/String;)Ljava/lang/String;", false));
-                        status.addInjection();
-                        break;
-                    }
-                }
-            }
-
             if (checkMethodNode(methodNode, MethodMapping.RENDERSCOREBOARD)) {
                 for (final AbstractInsnNode insnNode : methodNode.instructions.toArray()) {
                     if (checkMethodInsnNode(insnNode, MethodMapping.FORMATPLAYERNAME) && checkVarInsnNode(insnNode.getNext(), ASTORE, 15)) {
@@ -46,7 +34,6 @@ public class GuiIngameTransformer implements IMyClassTransformer {
                     }
                 }
             }
-
         }
         return classNode;
     }
