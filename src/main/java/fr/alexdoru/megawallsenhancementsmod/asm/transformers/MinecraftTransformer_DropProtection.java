6eee620b7c190271220fbcad504f54b55b03d393
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2022-10-12 21:51:59 +0200

    split MinecraftTransformer
    (cherry picked from commit a38d6e19078f1510be1d9380d210b41f3d2e926d)

---
+++ src/main/java/fr/alexdoru/megawallsenhancementsmod/asm/transformers/MinecraftTransformer_DropProtection.java
@@ -0,0 +1,72 @@
+package fr.alexdoru.megawallsenhancementsmod.asm.transformers;
+
+import fr.alexdoru.megawallsenhancementsmod.asm.*;
+import org.objectweb.asm.tree.*;
+
+import static org.objectweb.asm.Opcodes.*;
+
+public class MinecraftTransformer_DropProtection implements IMyClassTransformer {
+
+    @Override
+    public String getTargetClassName() {
+        return "net.minecraft.client.Minecraft";
+    }
+
+    @Override
+    public ClassNode transform(ClassNode classNode, InjectionStatus status) {
+        status.setInjectionPoints(3);
+
+        for (final MethodNode methodNode : classNode.methods) {
+            if (checkMethodNode(methodNode, MethodMapping.RUNTICK)) {
+                for (final AbstractInsnNode insnNode : methodNode.instructions.toArray()) {
+
+                    if (checkMethodInsnNode(insnNode, MethodMapping.CHANGECURRENTITEM)) {
+                        /*
+                         * Injects before line 1869
+                         * MinecraftHook.updateCurrentSlot(this);
+                         */
+                        methodNode.instructions.insertBefore(insnNode, updateCurrentSlotInsnList());
+                        status.addInjection();
+                    }
+
+                    if (checkFieldInsnNode(insnNode, PUTFIELD, FieldMapping.INVENTORYPLAYER$CURRENTITEM)) {
+                        /*
+                         * Injects before line 2077
+                         * MinecraftHook.updateCurrentSlot(this);
+                         */
+                        methodNode.instructions.insertBefore(insnNode, updateCurrentSlotInsnList());
+                        status.addInjection();
+                    }
+
+                    if (checkMethodInsnNode(insnNode, MethodMapping.DROPONEITEM)) {
+                        /*
+                         * Replaces line 2101 :
+                         * this.thePlayer.dropOneItem(GuiScreen.isCtrlKeyDown());
+                         * With :
+                         * MinecraftHook.dropOneItem(this.thePlayer);
+                         */
+                        methodNode.instructions.remove(insnNode.getPrevious());
+                        methodNode.instructions.remove(insnNode.getNext());
+                        final InsnList list = new InsnList();
+                        list.add(new MethodInsnNode(INVOKESTATIC, getHookClass("MinecraftHook"), "dropOneItem", "(L" + ClassMapping.ENTITYPLAYERSP + ";)V", false));
+                        methodNode.instructions.insertBefore(insnNode, list);
+                        methodNode.instructions.remove(insnNode);
+                        status.addInjection();
+                    }
+
+                }
+            }
+        }
+
+        return classNode;
+
+    }
+
+    private InsnList updateCurrentSlotInsnList() {
+        final InsnList list = new InsnList();
+        list.add(new VarInsnNode(ALOAD, 0));
+        list.add(new MethodInsnNode(INVOKESTATIC, getHookClass("MinecraftHook"), "updateCurrentSlot", "(L" + ClassMapping.MINECRAFT + ";)V", false));
+        return list;
+    }
+
+}
