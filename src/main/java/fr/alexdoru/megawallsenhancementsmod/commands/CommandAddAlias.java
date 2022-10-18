729315cc82265ff16ba24b17641f7facd23db204
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2022-10-18 17:15:08 +0200

    use dedicated class to hold the alias data

--- src/main/java/fr/alexdoru/megawallsenhancementsmod/commands/CommandAddAlias.java
+++ src/main/java/fr/alexdoru/megawallsenhancementsmod/commands/CommandAddAlias.java
@@ -1,8 +1,6 @@
 package fr.alexdoru.megawallsenhancementsmod.commands;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.reflect.TypeToken;
+import fr.alexdoru.megawallsenhancementsmod.data.AliasData;
 import fr.alexdoru.megawallsenhancementsmod.utils.ChatUtil;
 import fr.alexdoru.megawallsenhancementsmod.utils.NameUtil;
 import fr.alexdoru.megawallsenhancementsmod.utils.TabCompletionUtil;
@@ -13,21 +11,14 @@ import net.minecraft.command.ICommandSender;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumChatFormatting;
 
-import java.io.*;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 
 public class CommandAddAlias extends CommandBase {
 
-    private static final HashMap<String, String> renamingMap = new HashMap<>();
-    private static File alliasDataFile;
-
     public CommandAddAlias() {
-        alliasDataFile = new File(Minecraft.getMinecraft().mcDataDir, "config/alliasData.json");
-        readDataFromFile();
-        Runtime.getRuntime().addShutdownHook(new Thread(CommandAddAlias::writeDataToFile));
+        AliasData.init();
     }
 
     @Override
@@ -43,13 +34,13 @@ public class CommandAddAlias extends CommandBase {
     @Override
     public void processCommand(ICommandSender sender, String[] args) {
         if (args.length == 1 && args[0].equals("clearall")) {
-            renamingMap.clear();
+            AliasData.getMap().clear();
             ChatUtil.addChatMessage(EnumChatFormatting.GREEN + "Cleared alias for all players.");
             return;
         } else if (args.length == 1 && args[0].equals("list")) {
             ChatUtil.addChatMessage(EnumChatFormatting.GREEN + "In this lobby :\n");
             for (final NetworkPlayerInfo networkPlayerInfo : Minecraft.getMinecraft().getNetHandler().getPlayerInfoMap()) {
-                final String alias = renamingMap.get(networkPlayerInfo.getGameProfile().getName());
+                final String alias = AliasData.getAlias(networkPlayerInfo.getGameProfile().getName());
                 if (alias != null) {
                     ChatUtil.addChatMessage(NameUtil.getFormattedName(networkPlayerInfo.getGameProfile().getName()) + EnumChatFormatting.RESET + " (" + alias + ")");
                 }
@@ -61,12 +52,12 @@ public class CommandAddAlias extends CommandBase {
             return;
         }
         if (args[0].equals("remove")) {
-            renamingMap.remove(args[1]);
+            AliasData.removeAlias(args[1]);
             NameUtil.updateGameProfileAndName(args[1], true);
             ChatUtil.addChatMessage(EnumChatFormatting.GREEN + "Removed alias for " + EnumChatFormatting.GOLD + args[1]);
             return;
         }
-        renamingMap.put(args[0], args[1]);
+        AliasData.putAlias(args[0], args[1]);
         NameUtil.updateGameProfileAndName(args[0], true);
         ChatUtil.addChatMessage(EnumChatFormatting.GREEN + "Added alias for " + EnumChatFormatting.GOLD + args[0] + EnumChatFormatting.GREEN + " : " + EnumChatFormatting.GOLD + args[1]);
     }
@@ -86,37 +77,6 @@ public class CommandAddAlias extends CommandBase {
     @Override
     public List<String> getCommandAliases() {
         return Collections.singletonList("ad");
-    }
-
-    public static HashMap<String, String> getMap() {
-        return renamingMap;
-    }
-
-    private static void readDataFromFile() {
-        if (!alliasDataFile.exists()) {
-            return;
-        }
-        try {
-            final Gson gson = new Gson();
-            final HashMap<String, String> hashMap = gson.fromJson(new FileReader(alliasDataFile), new TypeToken<HashMap<String, String>>() {}.getType());
-            if (hashMap != null) {
-                renamingMap.putAll(hashMap);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void writeDataToFile() {
-        try {
-            final Gson gson = new GsonBuilder().serializeNulls().setPrettyPrinting().create();
-            final String jsonString = gson.toJson(renamingMap);
-            final BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(alliasDataFile));
-            bufferedWriter.write(jsonString);
-            bufferedWriter.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
 }
