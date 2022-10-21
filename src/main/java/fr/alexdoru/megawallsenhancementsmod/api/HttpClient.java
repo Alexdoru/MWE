78c73d638ce881f4d4a55dde1ac2053a06317f67
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2022-10-21 03:26:20 +0200

    changed how it handles error codes in HttpClient
    (cherry picked from commit 00efff3191f4d23dfdc5fecfc51c5e077dd1df79)

--- src/main/java/fr/alexdoru/megawallsenhancementsmod/api/HttpClient.java
+++ src/main/java/fr/alexdoru/megawallsenhancementsmod/api/HttpClient.java
@@ -11,36 +11,39 @@ import java.nio.charset.StandardCharsets;
 
 public class HttpClient {
 
-    private String rawResponse;
+    private final String rawResponse;
 
     public HttpClient(String url) throws ApiException {
 
         try {
+
             final HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
             connection.setRequestMethod("GET");
             connection.setConnectTimeout(10000);
             connection.setReadTimeout(10000);
             connection.addRequestProperty("User-Agent", "Alexdoru's Mega Walls Enhancements Mod");
-
             final int status = connection.getResponseCode();
 
-            if (status == 200) { //connection successfull
-
-                final BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8));
-                final StringBuilder responsecontent = new StringBuilder();
-                String line;
-
-                while ((line = reader.readLine()) != null) {
-                    responsecontent.append(line);
+            if (status != 200) {
+                if (url.contains("api.hypixel.net")) {
+                    if (status == 429) {
+                        throw new ApiException("Exceeding amount of requests per minute allowed by Hypixel");
+                    } else if (status == 403) {
+                        throw new ApiException("Invalid API key");
+                    }
                 }
-                reader.close();
-                this.rawResponse = responsecontent.toString();
-
-            } else if (status == 429 && url.contains("api.hypixel.net")) {
-                throw new ApiException("Exceeding amount of requests per minute allowed by Hypixel");
-            } else if (status == 403 && url.contains("api.hypixel.net")) {
-                throw new ApiException("Invalid API key");
+                throw new ApiException("Http error code : " + status);
             }
+
+            final BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8));
+            final StringBuilder responseContent = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                responseContent.append(line);
+            }
+            reader.close();
+            this.rawResponse = responseContent.toString();
+
         } catch (IOException e) {
             e.printStackTrace();
             throw new ApiException("An error occured while contacting the Api");
