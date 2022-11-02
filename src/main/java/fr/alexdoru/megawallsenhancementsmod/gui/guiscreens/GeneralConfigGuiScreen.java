815304f9216efb729df7a8be217ef9af1c0bc4a4
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2022-11-02 03:18:29 +0100

    create a HUD config gui screen

--- src/main/java/fr/alexdoru/megawallsenhancementsmod/gui/guiscreens/GeneralConfigGuiScreen.java
+++ src/main/java/fr/alexdoru/megawallsenhancementsmod/gui/guiscreens/GeneralConfigGuiScreen.java
@@ -15,23 +15,23 @@ public class GeneralConfigGuiScreen extends MyGuiScreen {
          * Defines the button list
          */
         final int buttonsWidth = 150;
-        this.buttonList.add(new GuiButton(0, getxCenter() - buttonsWidth / 2, getButtonYPos(0), buttonsWidth, ButtonsHeight, EnumChatFormatting.AQUA + "Final Kill Counter"));
-        this.buttonList.add(new GuiButton(1, getxCenter() - buttonsWidth / 2, getButtonYPos(1), buttonsWidth, ButtonsHeight, EnumChatFormatting.GREEN + "Mega Walls Enhancements"));
-        this.buttonList.add(new GuiButton(2, getxCenter() - buttonsWidth / 2, getButtonYPos(2), buttonsWidth, ButtonsHeight, EnumChatFormatting.RED + "No Cheaters"));
-        this.buttonList.add(new GuiButton(5, getxCenter() + buttonsWidth, getButtonYPos(2), buttonsWidth, ButtonsHeight, EnumChatFormatting.WHITE + "Automatic Updates : " + getSuffix(ConfigHandler.automaticUpdate)));
-        this.buttonList.add(new GuiButton(4, getxCenter() - buttonsWidth / 2, getButtonYPos(3), buttonsWidth, ButtonsHeight, EnumChatFormatting.BLUE + "Hitboxes, better F3+b"));
-        this.buttonList.add(new GuiButton(3, getxCenter() - buttonsWidth / 2, getButtonYPos(5), buttonsWidth, ButtonsHeight, "Close"));
+        this.buttonList.add(new GuiButton(0, getxCenter() - buttonsWidth / 2, getButtonYPos(1), buttonsWidth, ButtonsHeight, EnumChatFormatting.AQUA + "Final Kill Counter"));
+        this.buttonList.add(new GuiButton(1, getxCenter() - buttonsWidth / 2, getButtonYPos(2), buttonsWidth, ButtonsHeight, EnumChatFormatting.GREEN + "Mega Walls Enhancements"));
+        this.buttonList.add(new GuiButton(5, getxCenter() - buttonsWidth / 2, getButtonYPos(3), buttonsWidth, ButtonsHeight, EnumChatFormatting.DARK_PURPLE + "HUDs"));
+        this.buttonList.add(new GuiButton(2, getxCenter() - buttonsWidth / 2, getButtonYPos(4), buttonsWidth, ButtonsHeight, EnumChatFormatting.RED + "No Cheaters"));
+        this.buttonList.add(new GuiButton(6, getxCenter() + buttonsWidth, getButtonYPos(2), buttonsWidth, ButtonsHeight, EnumChatFormatting.WHITE + "Automatic Updates : " + getSuffix(ConfigHandler.automaticUpdate)));
+        this.buttonList.add(new GuiButton(4, getxCenter() - buttonsWidth / 2, getButtonYPos(5), buttonsWidth, ButtonsHeight, EnumChatFormatting.BLUE + "Hitboxes, better F3+b"));
+        this.buttonList.add(new GuiButton(3, getxCenter() - buttonsWidth / 2, getButtonYPos(7), buttonsWidth, ButtonsHeight, "Close"));
     }
 
     @Override
     public void drawScreen(int mouseX, int mouseY, float partialTicks) {
-        drawCenteredTitle(EnumChatFormatting.GREEN + "Config", 2, (width / 2f), getButtonYPos(-1), 0);
+        drawCenteredTitle(EnumChatFormatting.GREEN + "Config", 2, getxCenter(), getButtonYPos(-1), 0);
         super.drawScreen(mouseX, mouseY, partialTicks);
     }
 
     @Override
     protected void actionPerformed(GuiButton button) throws IOException {
-
         switch (button.id) {
             case 0:
                 mc.displayGuiScreen(new FKConfigGuiScreen(this));
@@ -49,12 +49,14 @@ public class GeneralConfigGuiScreen extends MyGuiScreen {
                 mc.displayGuiScreen(new HitboxConfigGuiScreen(this));
                 break;
             case 5:
+                mc.displayGuiScreen(new HUDsConfigGuiScreen(this));
+                break;
+            case 6:
                 ConfigHandler.automaticUpdate = !ConfigHandler.automaticUpdate;
                 button.displayString = EnumChatFormatting.WHITE + "Automatic Updates : " + getSuffix(ConfigHandler.automaticUpdate);
                 break;
         }
         super.actionPerformed(button);
-
     }
 
 }
