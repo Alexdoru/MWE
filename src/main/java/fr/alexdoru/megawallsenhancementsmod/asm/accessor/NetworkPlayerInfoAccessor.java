62f969954133aed8b5ab32ecb2b231bee7a0e182
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2022-11-04 16:55:42 +0100

    add getter for finals in NetworkPlayerInfoAccessor
    (cherry picked from commit 7417c57cad1538eee89be7fd2afd1c84400bc010)

--- src/main/java/fr/alexdoru/megawallsenhancementsmod/asm/accessor/NetworkPlayerInfoAccessor.java
+++ src/main/java/fr/alexdoru/megawallsenhancementsmod/asm/accessor/NetworkPlayerInfoAccessor.java
@@ -4,5 +4,6 @@ import net.minecraft.util.IChatComponent;
 
 public interface NetworkPlayerInfoAccessor {
     void setPlayerFinalkills(int playerFinalkills);
+    int getPlayerFinalkills();
     void setCustomDisplayname(IChatComponent customDisplaynameIn);
 }
