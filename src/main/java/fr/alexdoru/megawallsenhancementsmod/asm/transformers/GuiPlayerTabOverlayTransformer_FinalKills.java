8c0f90aef9c1b4acb900deca14c6451f9ad1cb69
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2022-11-08 02:13:22 +0100

    render the finals in tablist with correct width and outside the render score method, will render finals pre-dm

--- src/main/java/fr/alexdoru/megawallsenhancementsmod/asm/transformers/GuiPlayerTabOverlayTransformer_FinalKills.java
+++ src/main/java/fr/alexdoru/megawallsenhancementsmod/asm/transformers/GuiPlayerTabOverlayTransformer_FinalKills.java
@@ -17,57 +17,87 @@ public class GuiPlayerTabOverlayTransformer_FinalKills implements IMyClassTransf
 
     @Override
     public ClassNode transform(ClassNode classNode, InjectionStatus status) {
-
-        status.setInjectionPoints(2);
-
+        status.setInjectionPoints(5);
         for (final MethodNode methodNode : classNode.methods) {
-
             if (checkMethodNode(methodNode, MethodMapping.RENDERPLAYERLIST)) {
+                methodNode.instructions.insert(methodNode.instructions.getFirst(), new MethodInsnNode(INVOKESTATIC, getHookClass("GuiPlayerTabOverlayHook"), "resetFinalsScoreWidth", "()V", false));
+                status.addInjection();
+                boolean injectedMax = false;
+                boolean lookFork5k2 = false;
+                int ordinal_getGameType = 0;
+                int index_k2 = -1;
+                int index_j2 = -1;
+                int index_i = -1;
+                int index_networkplayerinfo = -1;
+                AbstractInsnNode latestAload0 = null;
                 for (final AbstractInsnNode insnNode : methodNode.instructions.toArray()) {
-                    if (checkVarInsnNode(insnNode, ILOAD, 7) && checkVarInsnNode(insnNode.getNext(), ISTORE, 12)) {
-                        /*
-                         * Replaces line 109 :
-                         * l = j;
-                         * With :
-                         * l = j + GuiPlayerTabOverlayHook.getFKScoreWidth();
-                         */
+                    if (!injectedMax && checkMethodInsnNode(insnNode, MethodMapping.MATH$MAX)) {
+                        final AbstractInsnNode nextNode = insnNode.getNext();
+                        if (checkVarInsnNode(nextNode, ISTORE, 6)) {
+                            final InsnList list = new InsnList();
+                            list.add(new VarInsnNode(ALOAD, 9));
+                            list.add(new FieldInsnNode(GETFIELD, ClassMapping.NETWORKPLAYERINFO.toString(), "mwenhancements$playerFinalkills", "I"));
+                            list.add(new MethodInsnNode(INVOKESTATIC, getHookClass("GuiPlayerTabOverlayHook"), "computeFKScoreWidth", "(I)V", false));
+                            methodNode.instructions.insert(nextNode, list);
+                            status.addInjection();
+                            injectedMax = true;
+                        }
+                    } else if (checkVarInsnNode(insnNode, ILOAD, 7) && checkVarInsnNode(insnNode.getNext(), ISTORE, 12) ||
+                            checkInsnNode(insnNode, ICONST_0) && checkVarInsnNode(insnNode.getNext(), ISTORE, 12)) {
                         final InsnList list = new InsnList();
-                        list.add(new MethodInsnNode(INVOKESTATIC, getHookClass("GuiPlayerTabOverlayHook"), "getFKScoreWidth", "()I", false));
+                        list.add(new MethodInsnNode(INVOKESTATIC, getHookClass("GuiPlayerTabOverlayHook"), "getRenderScoreWidth", "()I", false));
                         list.add(new InsnNode(IADD));
                         methodNode.instructions.insert(insnNode, list);
                         status.addInjection();
-                    }
-                }
-            }
-
-            if (checkMethodNode(methodNode, MethodMapping.DRAWSCOREBOARDVALUES)) {
-                int ordinal = 0;
-                for (final AbstractInsnNode insnNode : methodNode.instructions.toArray()) {
-                    if (checkMethodInsnNode(insnNode, MethodMapping.DRAWSTRINGWITHSHADOW)) {
-                        final AbstractInsnNode nextNode = insnNode.getNext();
-                        if (ordinal == 1 && checkInsnNode(nextNode, POP)) {
-                            /*
-                            Injects after line 365 :
-                            GuiPlayerTabOverlayHook.renderFinals(p_175247_6_.playerFinalkills, p_175247_5_, p_175247_2_);
-                             */
-                            final InsnList list = new InsnList();
-                            list.add(new VarInsnNode(ALOAD, 6)); // load networkplayerinfo / p_175247_6_
-                            list.add(new FieldInsnNode(GETFIELD, ClassMapping.NETWORKPLAYERINFO.toString(), "mwenhancements$playerFinalkills", "I")); // get playerFinalkills field
-                            list.add(new VarInsnNode(ILOAD, 5));
-                            list.add(new VarInsnNode(ILOAD, 2));
-                            list.add(new MethodInsnNode(INVOKESTATIC, getHookClass("GuiPlayerTabOverlayHook"), "renderFinals", "(III)V", false));
-                            methodNode.instructions.insert(nextNode, list);
-                            status.addInjection();
+                    } else if (lookFork5k2 && checkVarInsnNode(insnNode, ILOAD)) {
+                        final AbstractInsnNode secondNode = insnNode.getNext();
+                        if (checkVarInsnNode(secondNode, ILOAD)) {
+                            final AbstractInsnNode thirdNode = secondNode.getNext();
+                            if (checkInsnNode(thirdNode, IADD)) {
+                                final AbstractInsnNode fourthNode = thirdNode.getNext();
+                                if (checkInsnNode(fourthNode, ICONST_1)) {
+                                    final AbstractInsnNode fifthNode = fourthNode.getNext();
+                                    if (checkInsnNode(fifthNode, IADD)) {
+                                        final AbstractInsnNode sixthNode = fifthNode.getNext();
+                                        if (checkVarInsnNode(sixthNode, ISTORE)) {
+                                            index_j2 = ((VarInsnNode) insnNode).var;
+                                            index_i = ((VarInsnNode) secondNode).var;
+                                        }
+                                    }
+                                }
+                            }
                         }
-                        ordinal++;
+                    } else if (lookFork5k2 && checkVarInsnNode(insnNode, ALOAD, 3)) {
+                        final AbstractInsnNode nextNode = insnNode.getNext();
+                        if (checkVarInsnNode(nextNode, ILOAD)) {
+                            index_k2 = ((VarInsnNode) nextNode).var;
+                        }
+                    } else if (ordinal_getGameType < 2 && checkMethodInsnNode(insnNode, MethodMapping.NETWORKPLAYERINFO$GETGAMETYPE)) {
+                        ordinal_getGameType++;
+                        lookFork5k2 = ordinal_getGameType == 2;
+                    } else if (checkMethodInsnNode(insnNode, MethodMapping.DRAWSCOREBOARDVALUES)) {
+                        lookFork5k2 = false;
+                        final AbstractInsnNode previousNode = insnNode.getPrevious();
+                        if (checkVarInsnNode(previousNode, ALOAD)) {
+                            index_networkplayerinfo = ((VarInsnNode) previousNode).var;
+                        }
+                    } else if (latestAload0 != null && index_j2 != -1 && index_i != -1 && index_k2 != -1 && index_networkplayerinfo != -1 && checkMethodInsnNode(insnNode, MethodMapping.GUIPLAYERTABOVERLAY$DRAWPING)) {
+                        final InsnList list = new InsnList();
+                        list.add(new VarInsnNode(ALOAD, index_networkplayerinfo));
+                        list.add(new FieldInsnNode(GETFIELD, ClassMapping.NETWORKPLAYERINFO.toString(), "mwenhancements$playerFinalkills", "I"));
+                        list.add(new VarInsnNode(ILOAD, index_j2));
+                        list.add(new VarInsnNode(ILOAD, index_i));
+                        list.add(new VarInsnNode(ILOAD, index_k2));
+                        list.add(new MethodInsnNode(INVOKESTATIC, getHookClass("GuiPlayerTabOverlayHook"), "renderFinals", "(IIII)V", false));
+                        methodNode.instructions.insertBefore(latestAload0, list);
+                        status.addInjection();
+                    } else if (checkVarInsnNode(insnNode, ALOAD, 0)) {
+                        latestAload0 = insnNode;
                     }
                 }
             }
-
         }
-
         return classNode;
-
     }
 
 }
