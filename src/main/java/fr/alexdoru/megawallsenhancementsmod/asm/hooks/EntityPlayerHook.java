89e2d18ef4589e038208c58a8dc84ad3297e931d
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2022-11-08 13:14:35 +0100

    fix crash with asm for prestige 5

--- src/main/java/fr/alexdoru/megawallsenhancementsmod/asm/hooks/EntityPlayerHook.java
+++ src/main/java/fr/alexdoru/megawallsenhancementsmod/asm/hooks/EntityPlayerHook.java
@@ -1,13 +1,12 @@
 package fr.alexdoru.megawallsenhancementsmod.asm.hooks;
 
-import com.mojang.authlib.GameProfile;
 import fr.alexdoru.megawallsenhancementsmod.asm.accessor.EntityPlayerAccessor;
 import net.minecraft.entity.player.EntityPlayer;
 
 @SuppressWarnings("unused")
 public class EntityPlayerHook {
 
-    public static String getTransformedDisplayName(String displayNameIn, EntityPlayer entityIn, GameProfile gameProfile) {
+    public static String getTransformedDisplayName(String displayNameIn, EntityPlayer entityIn) {
         final String prestige4Tag = ((EntityPlayerAccessor) entityIn).getPrestige4Tag();
         final String prestige5Tag = ((EntityPlayerAccessor) entityIn).getPrestige5Tag();
         if (prestige5Tag != null && prestige4Tag != null) {
