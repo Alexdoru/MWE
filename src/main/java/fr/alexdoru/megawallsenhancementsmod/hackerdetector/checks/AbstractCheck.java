2cf67cf77c4d0fe743f08a245a7171aefc275efa
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2022-11-30 04:03:04 +0100

    added util fonction to get time to break a block in AbstractCheck
    (cherry picked from commit a17eec4699a287d3dc6b4adf67e05821f8285457)

--- src/main/java/fr/alexdoru/megawallsenhancementsmod/hackerdetector/checks/AbstractCheck.java
+++ src/main/java/fr/alexdoru/megawallsenhancementsmod/hackerdetector/checks/AbstractCheck.java
@@ -4,14 +4,18 @@ import fr.alexdoru.megawallsenhancementsmod.chat.ChatUtil;
 import fr.alexdoru.megawallsenhancementsmod.hackerdetector.data.PlayerDataSamples;
 import fr.alexdoru.megawallsenhancementsmod.hackerdetector.utils.ViolationLevelTracker;
 import fr.alexdoru.megawallsenhancementsmod.utils.NameUtil;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.event.ClickEvent;
 import net.minecraft.event.HoverEvent;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.BlockPos;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.ChatStyle;
 import net.minecraft.util.EnumChatFormatting;
+import net.minecraft.world.World;
+import net.minecraftforge.common.ForgeHooks;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -31,25 +35,6 @@ public abstract class AbstractCheck implements ICheck {
                 flag(player, this.getCheatName(), this.getCheatDescription());
             }
         }
-    }
-
-    /**
-     * Returns the base speed for a player sprinting without jumping.
-     * Without speed, jumping while sprinting is ~29% faster than normal sprinting
-     * That makes normal sprinting ~21% slower than sprint while jumping
-     * Result is in meters per tick
-     */
-    protected static double getBaseSprintingSpeed(EntityPlayer player) {
-        int speedAmplifier = 0;
-        if (player.isPotionActive(Potion.moveSpeed)) {
-            final PotionEffect activePotionEffect = player.getActivePotionEffect(Potion.moveSpeed);
-            speedAmplifier = activePotionEffect.getAmplifier() + 1;
-        }
-        return getBaseSprintingSpeed(speedAmplifier);
-    }
-
-    protected static double getBaseSprintingSpeed(int speedAmplifier) {
-        return 0.2806d * (1.0d + 0.2d * speedAmplifier);
     }
 
     protected static void flag(EntityPlayer player, String cheat, String cheatDescription) {
@@ -99,6 +84,56 @@ public abstract class AbstractCheck implements ICheck {
 
     protected static void debug(String msg) {
         ChatUtil.debug(msg);
+    }
+
+    /**
+     * Returns the base speed for a player sprinting without jumping.
+     * Without speed, jumping while sprinting is ~29% faster than normal sprinting
+     * That makes normal sprinting ~21% slower than sprint while jumping
+     * Result is in meters per tick
+     */
+    protected static double getBaseSprintingSpeed(EntityPlayer player) {
+        int speedAmplifier = 0;
+        if (player.isPotionActive(Potion.moveSpeed)) {
+            final PotionEffect activePotionEffect = player.getActivePotionEffect(Potion.moveSpeed);
+            speedAmplifier = activePotionEffect.getAmplifier() + 1;
+        }
+        return getBaseSprintingSpeed(speedAmplifier);
+    }
+
+    protected static double getBaseSprintingSpeed(int speedAmplifier) {
+        return 0.2806d * (1.0d + 0.2d * speedAmplifier);
+    }
+
+    /**
+     * Returns the amount of ticks for a certain player to break a certain block
+     * The enchantements of the tool must be readable from the client for this to be accurate
+     * This accounts for the 5 ticks cooldown there is after breaking a bloc
+     * Returns -1 if the block isn't harvestable
+     */
+    protected static int getTimeToHarvestBlock(IBlockState state, EntityPlayer player, World world, BlockPos pos) {
+        return getTimeToHarvestBlock(ForgeHooks.blockStrength(state, player, world, pos));
+    }
+
+    /**
+     * Returns the amount of ticks for a certain player to break a certain block
+     * The enchantements of the tool must be readable from the client for this to be accurate
+     * This accounts for the 5 ticks cooldown there is after breaking a bloc
+     * Returns -1 if the block isn't harvestable
+     */
+    protected static int getTimeToHarvestBlock(float blockStrength) {
+        if (blockStrength <= 0) {
+            return -1;
+        }
+        int i = 1;
+        if (blockStrength < 1.0F) {
+            float breakProgression = 0F;
+            while (breakProgression < 1.0f) {
+                i++;
+                breakProgression += blockStrength;
+            }
+        }
+        return i == 1 ? i : i + 5;
     }
 
 }
