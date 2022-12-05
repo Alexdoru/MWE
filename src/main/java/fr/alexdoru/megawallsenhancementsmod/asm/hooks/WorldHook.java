8aa07f40d75bfac69670136a4d42e3ce754fc383
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2022-12-05 12:54:30 +0100

    add config for hacker detector
    (cherry picked from commit 5d49547bd7dd6c51d508d5fe48f060650787541e)

--- src/main/java/fr/alexdoru/megawallsenhancementsmod/asm/hooks/WorldHook.java
+++ src/main/java/fr/alexdoru/megawallsenhancementsmod/asm/hooks/WorldHook.java
@@ -1,5 +1,6 @@
 package fr.alexdoru.megawallsenhancementsmod.asm.hooks;
 
+import fr.alexdoru.megawallsenhancementsmod.config.ConfigHandler;
 import fr.alexdoru.megawallsenhancementsmod.hackerdetector.HackerDetector;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
@@ -8,7 +9,7 @@ import net.minecraft.world.World;
 @SuppressWarnings("unused")
 public class WorldHook {
     public static void performChecksOnEntity(World world, Entity entity) {
-        if (world.isRemote && entity instanceof EntityPlayer) {
+        if (ConfigHandler.hackerDetector && world.isRemote && entity instanceof EntityPlayer) {
             HackerDetector.INSTANCE.performChecksOnPlayer((EntityPlayer) entity);
         }
     }
