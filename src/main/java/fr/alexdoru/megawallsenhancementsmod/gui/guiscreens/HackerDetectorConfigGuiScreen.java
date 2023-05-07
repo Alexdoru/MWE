044e9d14a3114b2410bce6a36270532c15f6dbc8
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2023-05-07 03:13:06 +0200

    use the new buttons in the HackerDetectorConfigGuiScreen
    (cherry picked from commit 515905b5105c3788983d76e2923d0712271ffd45)

--- src/main/java/fr/alexdoru/megawallsenhancementsmod/gui/guiscreens/HackerDetectorConfigGuiScreen.java
+++ src/main/java/fr/alexdoru/megawallsenhancementsmod/gui/guiscreens/HackerDetectorConfigGuiScreen.java
@@ -1,17 +1,13 @@
 package fr.alexdoru.megawallsenhancementsmod.gui.guiscreens;
 
 import fr.alexdoru.megawallsenhancementsmod.config.ConfigHandler;
-import net.minecraft.client.gui.GuiButton;
+import fr.alexdoru.megawallsenhancementsmod.gui.elements.OptionGuiButton;
+import fr.alexdoru.megawallsenhancementsmod.gui.elements.SimpleGuiButton;
+import fr.alexdoru.megawallsenhancementsmod.gui.elements.TextElement;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.util.EnumChatFormatting;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
 public class HackerDetectorConfigGuiScreen extends MyGuiScreen {
-
-    private final String msg = EnumChatFormatting.WHITE + "Disclaimer : this is not 100% accurate and can sometimes flag legit players,";
 
     public HackerDetectorConfigGuiScreen(GuiScreen parent) {
         this.parent = parent;
@@ -19,109 +15,53 @@ public class HackerDetectorConfigGuiScreen extends MyGuiScreen {
 
     @Override
     public void initGui() {
-        final int buttonsWidth = 200;
+        final String msg = EnumChatFormatting.WHITE + "Disclaimer : this is not 100% accurate and can sometimes flag legit players,";
         this.maxWidth = fontRendererObj.getStringWidth(msg);
         this.maxHeight = (buttonsHeight + 4) * 10 + buttonsHeight;
         super.initGui();
-        final int xPos = getxCenter() - buttonsWidth / 2;
-        this.buttonList.add(new GuiButton(0, xPos, getButtonYPos(2), buttonsWidth, buttonsHeight, getButtonDisplayString(0))); //hackerdetector toggle
-        this.buttonList.add(new GuiButton(1, xPos, getButtonYPos(3), buttonsWidth, buttonsHeight, getButtonDisplayString(1))); //save in nocheaters toggle
-        this.buttonList.add(new GuiButton(2, xPos, getButtonYPos(4), buttonsWidth, buttonsHeight, getButtonDisplayString(2))); //report flagged players toggle
-        this.buttonList.add(new GuiButton(3, xPos, getButtonYPos(5), buttonsWidth, buttonsHeight, getButtonDisplayString(3))); //show flag messages toggle
-        this.buttonList.add(new GuiButton(4, xPos, getButtonYPos(6), buttonsWidth, buttonsHeight, getButtonDisplayString(4))); //compact flag messages toggle
-        this.buttonList.add(new GuiButton(5, xPos, getButtonYPos(7), buttonsWidth, buttonsHeight, getButtonDisplayString(5))); //show single flag message toggle
-        this.buttonList.add(new GuiButton(6, getxCenter() - 150 / 2, getButtonYPos(9), 150, buttonsHeight, getButtonDisplayString(6))); //done button
-    }
-
-    @Override
-    public void drawScreen(int mouseX, int mouseY, float partialTicks) {
-        drawCenteredTitle(EnumChatFormatting.DARK_RED + "Hacker Detector", 2, getxCenter(), getButtonYPos(-1));
-        drawCenteredString(fontRendererObj, msg, getxCenter(), getButtonYPos(0), 0);
-        final String msg0 = EnumChatFormatting.WHITE + "it won't flag every cheater either, however players that";
-        drawCenteredString(fontRendererObj, msg0, getxCenter(), getButtonYPos(0) + fontRendererObj.FONT_HEIGHT, 0);
-        final String msg1 = EnumChatFormatting.WHITE + "are regularly flagging are definitely cheating";
-        drawCenteredString(fontRendererObj, msg1, getxCenter(), getButtonYPos(0) + 2 * fontRendererObj.FONT_HEIGHT, 0);
-        super.drawScreen(mouseX, mouseY, partialTicks);
-        drawTooltips(mouseX, mouseY);
-    }
-
-    private String getButtonDisplayString(int id) {
-        switch (id) {
-            case 0:
-                return "Hacker Detector : " + getSuffix(ConfigHandler.hackerDetector);
-            case 1:
-                return "Save in NoCheaters : " + getSuffix(ConfigHandler.addToReportList);
-            case 2:
-                return "Report Flagged players : " + getSuffix(ConfigHandler.autoreportFlaggedPlayers);
-            case 3:
-                return "Show flag messages : " + getSuffix(ConfigHandler.showFlagMessages);
-            case 4:
-                return "Compact flag messages : " + getSuffix(ConfigHandler.compactFlagMessages);
-            case 5:
-                return "Show single flag message : " + getSuffix(ConfigHandler.oneFlagMessagePerGame);
-            case 6:
-                return parent == null ? "Close" : "Done";
-            default:
-                return "invalid button id";
-        }
-    }
-
-    @Override
-    protected List<String> getTooltipText(int id) {
-        final List<String> textLines = new ArrayList<>();
-        switch (id) {
-            case 0:
-                textLines.add(EnumChatFormatting.GREEN + "Analyses movements and actions of players in your game and tells you if they are cheating");
-                textLines.add("");
-                textLines.add(EnumChatFormatting.GREEN + "Currently has checks for : " + EnumChatFormatting.YELLOW + "Autoblock, Fastbreak, Keepsprint, Noslowdown");
-                break;
-            case 1:
-                textLines.add(EnumChatFormatting.GREEN + "Saves flagged players in NoCheaters");
-                break;
-            case 2:
-                textLines.add(EnumChatFormatting.GREEN + "Sends a report automatically to Hypixel when it flags a cheater");
-                textLines.add(EnumChatFormatting.GRAY + "Only works in Mega Walls, sends one report per game per player");
-                break;
-            case 3:
-                textLines.add(EnumChatFormatting.GREEN + "Prints a message in chat when it detects a player using cheats");
-                break;
-            case 4:
-                textLines.add(EnumChatFormatting.GREEN + "Compacts identical flag messages together");
-                break;
-            case 5:
-                textLines.add(EnumChatFormatting.GREEN + "Print flag messages only once per game per player");
-                break;
-        }
-        return textLines;
-    }
-
-    @Override
-    protected void actionPerformed(GuiButton button) throws IOException {
-        switch (button.id) {
-            case 0:
-                ConfigHandler.hackerDetector = !ConfigHandler.hackerDetector;
-                break;
-            case 1:
-                ConfigHandler.addToReportList = !ConfigHandler.addToReportList;
-                break;
-            case 2:
-                ConfigHandler.autoreportFlaggedPlayers = !ConfigHandler.autoreportFlaggedPlayers;
-                break;
-            case 3:
-                ConfigHandler.showFlagMessages = !ConfigHandler.showFlagMessages;
-                break;
-            case 4:
-                ConfigHandler.compactFlagMessages = !ConfigHandler.compactFlagMessages;
-                break;
-            case 5:
-                ConfigHandler.oneFlagMessagePerGame = !ConfigHandler.oneFlagMessagePerGame;
-                break;
-            case 6:
-                mc.displayGuiScreen(parent);
-                break;
-        }
-        button.displayString = getButtonDisplayString(button.id);
-        super.actionPerformed(button);
+        final int xPos = getxCenter() - BUTTON_WIDTH / 2;
+        this.elementList.add(new TextElement(EnumChatFormatting.DARK_RED + "Hacker Detector", getxCenter(), getButtonYPos(-1)).setSize(2).makeCentered());
+        this.elementList.add(new TextElement(msg, getxCenter(), getButtonYPos(0)).makeCentered());
+        this.elementList.add(new TextElement(EnumChatFormatting.WHITE + "it won't flag every cheater either, however players that", getxCenter(), getButtonYPos(0) + fontRendererObj.FONT_HEIGHT).makeCentered());
+        this.elementList.add(new TextElement(EnumChatFormatting.WHITE + "are regularly flagging are definitely cheating", getxCenter(), getButtonYPos(0) + 2 * fontRendererObj.FONT_HEIGHT).makeCentered());
+        this.buttonList.add(new OptionGuiButton(
+                xPos, getButtonYPos(2),
+                "Hacker Detector",
+                (b) -> ConfigHandler.hackerDetector = b,
+                () -> ConfigHandler.hackerDetector,
+                EnumChatFormatting.GRAY + "Analyses movements and actions of players in your game and tells you if they are cheating. Currently has checks for : Autoblock, Fastbreak, Keepsprint, Noslowdown"));
+        this.buttonList.add(new OptionGuiButton(
+                xPos, getButtonYPos(3),
+                "Save in NoCheaters",
+                (b) -> ConfigHandler.addToReportList = b,
+                () -> ConfigHandler.addToReportList,
+                EnumChatFormatting.GRAY + "Saves flagged players in NoCheaters"));
+        this.buttonList.add(new OptionGuiButton(
+                xPos, getButtonYPos(4),
+                "Report Flagged players",
+                (b) -> ConfigHandler.autoreportFlaggedPlayers = b,
+                () -> ConfigHandler.autoreportFlaggedPlayers,
+                EnumChatFormatting.GRAY + "Sends a report automatically to Hypixel when it flags a cheater",
+                EnumChatFormatting.YELLOW + "Only works in Mega Walls, sends one report per game per player"));
+        this.buttonList.add(new OptionGuiButton(
+                xPos, getButtonYPos(5),
+                "Show flag messages",
+                (b) -> ConfigHandler.showFlagMessages = b,
+                () -> ConfigHandler.showFlagMessages,
+                EnumChatFormatting.GRAY + "Prints a message in chat when it detects a player using cheats"));
+        this.buttonList.add(new OptionGuiButton(
+                xPos, getButtonYPos(6),
+                "Compact flag messages",
+                (b) -> ConfigHandler.compactFlagMessages = b,
+                () -> ConfigHandler.compactFlagMessages,
+                EnumChatFormatting.GRAY + "Compacts identical flag messages together"));
+        this.buttonList.add(new OptionGuiButton(
+                xPos, getButtonYPos(7),
+                "Show single flag message",
+                (b) -> ConfigHandler.oneFlagMessagePerGame = b,
+                () -> ConfigHandler.oneFlagMessagePerGame,
+                EnumChatFormatting.GRAY + "Print flag messages only once per game per player"));
+        this.buttonList.add(new SimpleGuiButton(getxCenter() - 150 / 2, getButtonYPos(9), 150, buttonsHeight, "Done", () -> mc.displayGuiScreen(this.parent)));
     }
 
 }
