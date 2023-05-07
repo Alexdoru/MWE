c54c7063d30704814a73626a5fb8b9acf8ba086e
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2023-05-07 03:12:28 +0200

    add FancyGuiButton and OptionGuiButton to make buttons with tooltips and text that changes when pressed
    (cherry picked from commit 4208e7d337789ad0bb51a7446ede271a6e2b5702)

---
+++ src/main/java/fr/alexdoru/megawallsenhancementsmod/gui/elements/FancyGuiButton.java
@@ -0,0 +1,63 @@
+package fr.alexdoru.megawallsenhancementsmod.gui.elements;
+
+import fr.alexdoru.megawallsenhancementsmod.utils.StringUtil;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class FancyGuiButton extends SimpleGuiButton {
+
+    private final Supplier<String> textSupplier;
+    private final List<String> tooltip;
+
+    public FancyGuiButton(int x, int y, Supplier<String> buttonTextSupplier, Runnable action, List<String> tooltip) {
+        this(x, y, 200, 20, buttonTextSupplier, action, tooltip);
+    }
+
+    public FancyGuiButton(int x, int y, int widthIn, int heightIn, Supplier<String> buttonTextSupplier, Runnable action, List<String> tooltip) {
+        super(x, y, widthIn, heightIn, buttonTextSupplier.get(), action);
+        this.textSupplier = buttonTextSupplier;
+        this.tooltip = wrapTooltipLines(tooltip);
+    }
+
+    public FancyGuiButton(int x, int y, int widthIn, int heightIn, Supplier<String> buttonTextSupplier, Runnable action, String... tooltipLines) {
+        super(x, y, widthIn, heightIn, buttonTextSupplier.get(), action);
+        this.textSupplier = buttonTextSupplier;
+        if (tooltipLines == null) {
+            this.tooltip = null;
+            return;
+        }
+        this.tooltip = wrapTooltipLines(new ArrayList<>(Arrays.asList(tooltipLines)));
+    }
+
+    @Override
+    public void onButtonPressed() {
+        super.onButtonPressed();
+        this.displayString = textSupplier.get();
+    }
+
+    public List<String> getTooltip() {
+        return this.tooltip;
+    }
+
+    private static List<String> wrapTooltipLines(List<String> tooltip) {
+        if (tooltip == null) {
+            return null;
+        }
+        final int firstWrappedLine = 1;
+        final int wrapLength = 40;
+        final List<String> list = new ArrayList<>();
+        for (int i = 0; i < tooltip.size(); i++) {
+            final String line = tooltip.get(i);
+            if (i >= firstWrappedLine) {
+                list.addAll(StringUtil.wrap(line, wrapLength));
+            } else {
+                list.add(line);
+            }
+        }
+        return list;
+    }
+
+}
