18a65ffc2a6d67d31aa6bb12ddf5d75a4bd20bf0
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2023-06-06 02:32:05 +0200

    prevent unnecessary skin loading from networkPlayerInfo.getLocationSkin() calls
    (cherry picked from commit 5b3750f21cd89398f6ebeb680dc19ffedd4ba1b0)

--- src/main/java/fr/alexdoru/megawallsenhancementsmod/utils/SkinUtil.java
+++ src/main/java/fr/alexdoru/megawallsenhancementsmod/utils/SkinUtil.java
@@ -1,19 +1,14 @@
 package fr.alexdoru.megawallsenhancementsmod.utils;
 
 import net.minecraft.client.network.NetworkPlayerInfo;
+import net.minecraft.client.resources.DefaultPlayerSkin;
 import net.minecraft.util.ResourceLocation;
 
 public class SkinUtil {
 
-    // Fields stolen from net.minecraft.client.resources.DefaultPlayerSkin
-    /** The default skin for the Steve model. */
-    private static final ResourceLocation TEXTURE_STEVE = new ResourceLocation("textures/entity/steve.png");
-    /** The default skin for the Alex model. */
-    private static final ResourceLocation TEXTURE_ALEX = new ResourceLocation("textures/entity/alex.png");
-
     public static ResourceLocation getSkin(NetworkPlayerInfo networkPlayerInfo) {
         if (networkPlayerInfo == null) {
-            return TEXTURE_STEVE;
+            return DefaultPlayerSkin.getDefaultSkinLegacy();
         }
         return networkPlayerInfo.getLocationSkin();
     }
