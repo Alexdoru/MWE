73647ad0dbc288242aa4b04c1f4b582a2082dd96
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2023-06-19 21:47:28 +0200

    remove ConcurentHashMap from PrestigeVCache
    (cherry picked from commit e799fedeefb592d0ace40e60f08d8e4f66248445)

--- src/main/java/fr/alexdoru/megawallsenhancementsmod/data/PrestigeVCache.java
+++ src/main/java/fr/alexdoru/megawallsenhancementsmod/data/PrestigeVCache.java
@@ -11,80 +11,70 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.util.EnumChatFormatting;
 
 import java.util.HashMap;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.Map;
 
 public class PrestigeVCache {
 
-    private static final ConcurrentHashMap<String, PlayerPrestigeData> prestigeDataMap = new ConcurrentHashMap<>();
+    private static final Map<String, PlayerPrestigeData> prestigeDataMap = new HashMap<>();
 
     public static void clearCache() {
         prestigeDataMap.clear();
     }
 
     public static EnumChatFormatting checkCacheAndUpdate(String uuid, String playername, String classTag) {
-
         final MWClass mwClass = MWClass.fromTag(classTag);
         if (mwClass == null) {
             return null;
         }
-
         final PlayerPrestigeData playerPrestigeData = prestigeDataMap.get(uuid);
-
         if (playerPrestigeData == null) {
             createPlayerPrestigeData(uuid, mwClass, playername);
             return null;
         }
-
         final EnumChatFormatting chatFormatting = playerPrestigeData.playersPrestigeColors.get(mwClass);
         if (chatFormatting == null) {
             createClassData(uuid, mwClass, playerPrestigeData, playername);
             return null;
         }
-
         return chatFormatting;
-
     }
 
     private static void createPlayerPrestigeData(String uuid, MWClass mwClass, String playername) {
-
         prestigeDataMap.put(uuid, new PlayerPrestigeData());
-
         MultithreadingUtil.addTaskToQueue(() -> {
-
             try {
                 final MegaWallsClassStats mwclassstats = new MegaWallsClassStats(CachedHypixelPlayerData.getPlayerData(uuid), mwClass.className);
-                final PlayerPrestigeData playerPrestigeData = new PlayerPrestigeData();
-                playerPrestigeData.addClass(mwClass, mwclassstats.getClasspoints());
-                prestigeDataMap.put(uuid, playerPrestigeData);
-                Minecraft.getMinecraft().addScheduledTask(() -> NameUtil.updateMWPlayerDataAndEntityData(playername, false));
+                Minecraft.getMinecraft().addScheduledTask(() -> {
+                    final PlayerPrestigeData playerPrestigeData = new PlayerPrestigeData();
+                    playerPrestigeData.addClass(mwClass, mwclassstats.getClasspoints());
+                    prestigeDataMap.put(uuid, playerPrestigeData);
+                    NameUtil.updateMWPlayerDataAndEntityData(playername, false);
+                });
             } catch (ApiException e) {
-                prestigeDataMap.put(uuid, new PlayerPrestigeData());
+                Minecraft.getMinecraft().addScheduledTask(() ->
+                        prestigeDataMap.put(uuid, new PlayerPrestigeData())
+                );
             }
-
             return null;
-
         });
-
     }
 
     private static void createClassData(String uuid, MWClass mwClass, PlayerPrestigeData playerPrestigeData, String playername) {
-
         playerPrestigeData.addClass(mwClass, 0);
-
         MultithreadingUtil.addTaskToQueue(() -> {
-
             try {
                 final MegaWallsClassStats mwclassstats = new MegaWallsClassStats(CachedHypixelPlayerData.getPlayerData(uuid), mwClass.className);
-                playerPrestigeData.addClass(mwClass, mwclassstats.getClasspoints());
-                Minecraft.getMinecraft().addScheduledTask(() -> NameUtil.updateMWPlayerDataAndEntityData(playername, false));
+                Minecraft.getMinecraft().addScheduledTask(() -> {
+                    playerPrestigeData.addClass(mwClass, mwclassstats.getClasspoints());
+                    NameUtil.updateMWPlayerDataAndEntityData(playername, false);
+                });
             } catch (ApiException e) {
-                playerPrestigeData.addClass(mwClass, 0);
+                Minecraft.getMinecraft().addScheduledTask(() ->
+                        playerPrestigeData.addClass(mwClass, 0)
+                );
             }
-
             return null;
-
         });
-
     }
 
 }
