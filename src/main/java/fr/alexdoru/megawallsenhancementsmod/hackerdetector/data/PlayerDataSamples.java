6db8b0a776dbf60e30c89ecf74bc7f250c6349c2
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2023-09-03 23:01:41 +0200

    remove useless fields from PlayerDataSamples

--- src/main/java/fr/alexdoru/megawallsenhancementsmod/hackerdetector/data/PlayerDataSamples.java
+++ src/main/java/fr/alexdoru/megawallsenhancementsmod/hackerdetector/data/PlayerDataSamples.java
@@ -5,10 +5,6 @@ import fr.alexdoru.megawallsenhancementsmod.hackerdetector.checks.*;
 import fr.alexdoru.megawallsenhancementsmod.hackerdetector.utils.ViolationLevelTracker;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemFood;
-import net.minecraft.item.ItemPotion;
-import net.minecraft.item.ItemStack;
 
 public class PlayerDataSamples {
 
@@ -25,8 +21,6 @@ public class PlayerDataSamples {
     /** True when we receive a swing packet from this entity during the last tick */
     public boolean hasSwung = false;
     public final SampleListZ swingList = new SampleListZ(20);
-    /** Amount of ticks since the list time the player ate or drank something */
-    public int lastEatDrinkTime = 40;
     /** True if the player's armor has been damaged during this tick, for instance when being attacked
      * Disabled as of right now, always false */
     public boolean armorDamaged = false;
@@ -38,7 +32,7 @@ public class PlayerDataSamples {
     /** Player attacked during this tick if any */
     public EntityPlayer targetedPlayer;
     public SampleListZ attackList = new SampleListZ(20);
-    /** True if the player has beend attacked by another player during this tick */
+    /** True if the player has been attacked by another player during this tick */
     public boolean hasBeenAttacked = false;
     public SampleListD speedXList = new SampleListD(20);
     public SampleListD speedYList = new SampleListD(20);
@@ -51,15 +45,6 @@ public class PlayerDataSamples {
     public final ViolationLevelTracker keepsprintVL = KeepsprintCheck.newViolationTracker();
     public final ViolationLevelTracker killAuraVL = KillAuraCheck.newViolationTracker();
     public final ViolationLevelTracker noSlowdownVL = NoSlowdownCheck.newViolationTracker();
-    ///** The player's look */
-    //public Vector3D lookVector = new Vector3D();
-    ///** The angle difference in 3D space of the player's look since last tick */
-    //public double lookAngleDiff = 0D;
-    ///** Variation of Yaw since last tick (which is in XZ plane) */
-    //public double dYaw = 0D;
-    //public boolean lastNonZerodYawPositive = true;
-    ///** Amount of ticks since dYaw changed sign */
-    //public int lastTime_dYawChangedSign = 0;
 
     public void ontickStart() {
         this.updatedThisTick = false;
@@ -72,13 +57,6 @@ public class PlayerDataSamples {
     }
 
     public void onTick(EntityPlayer player) {
-        final ItemStack itemStack = player.getItemInUse();
-        if (itemStack != null) {
-            final Item item = itemStack.getItem();
-            this.lastEatDrinkTime = item instanceof ItemFood || item instanceof ItemPotion ? 0 : this.lastEatDrinkTime + 1;
-        } else {
-            this.lastEatDrinkTime += 1;
-        }
         this.sprintTime = player.getEntityAttribute(SharedMonsterAttributes.movementSpeed).getModifier(HackerDetector.sprintingUUID) == null ? 0 : this.sprintTime + 1;
         this.useItemTime = player.isUsingItem() ? this.useItemTime + 1 : 0;
         this.lastHurtTime = player.hurtTime == 9 ? 0 : this.lastHurtTime + 1;
@@ -88,25 +66,6 @@ public class PlayerDataSamples {
         this.speedXList.add((player.posX - player.lastTickPosX) * 20D);
         this.speedYList.add((player.posY - player.lastTickPosY) * 20D);
         this.speedZList.add((player.posZ - player.lastTickPosZ) * 20D);
-        //final Vector3D lookVector = Vector3D.getPlayersLookVec(player);
-        //this.lookAngleDiff = lookVector.getAngleWithVector(this.lookVector);
-        //this.lookVector = lookVector;
-        //final double dYaw = player.rotationYawHead - player.prevRotationYawHead;
-        //if (dYaw != 0 && this.dYaw != 0) {
-        //    this.lastTime_dYawChangedSign = dYaw * this.dYaw > 0 ? this.lastTime_dYawChangedSign + 1 : 0;
-        //} else if (dYaw != 0) {
-        //    if (this.lastNonZerodYawPositive) {
-        //        this.lastTime_dYawChangedSign = dYaw > 0 ? this.lastTime_dYawChangedSign + 1 : 0;
-        //    } else {
-        //        this.lastTime_dYawChangedSign = dYaw < 0 ? this.lastTime_dYawChangedSign + 1 : 0;
-        //    }
-        //} else {
-        //    this.lastTime_dYawChangedSign = this.lastTime_dYawChangedSign + 1;
-        //}
-        //if (dYaw != 0) {
-        //    this.lastNonZerodYawPositive = dYaw > 0;
-        //}
-        //this.dYaw = dYaw;
     }
 
     /** True if the player's position in the XZ plane is identical to the last tick */
