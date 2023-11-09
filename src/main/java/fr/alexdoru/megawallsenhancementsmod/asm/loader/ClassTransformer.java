dd3d6e26dcd18b476e4c83feb83e1a12189e3fd9
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2023-11-09 17:25:04 +0100

    patch autoblock bypass

--- src/main/java/fr/alexdoru/megawallsenhancementsmod/asm/loader/ClassTransformer.java
+++ src/main/java/fr/alexdoru/megawallsenhancementsmod/asm/loader/ClassTransformer.java
@@ -35,6 +35,7 @@ public class ClassTransformer implements IClassTransformer {
         registerTransformer(new CommandHandlerTransformer());
         registerTransformer(new EntityArrowTransformer());
         registerTransformer(new EntityFXTransformer());
+        registerTransformer(new EntityOtherPlayerMPTransformer_FixAutoblockBypass());
         registerTransformer(new EntityPlayerTransformer());
         registerTransformer(new EntityPlayerSPTransformer_CommandListener());
         //registerTransformer(new EntityPlayerSPTransformer_HealthListener());
