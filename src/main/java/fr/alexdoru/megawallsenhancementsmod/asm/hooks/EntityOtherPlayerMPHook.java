ce4dc35a883290d00bf3d99c7cadbb09440d8080
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2023-11-09 17:25:04 +0100

    patch autoblock bypass

---
+++ src/main/java/fr/alexdoru/megawallsenhancementsmod/asm/hooks/EntityOtherPlayerMPHook.java
@@ -0,0 +1,27 @@
+package fr.alexdoru.megawallsenhancementsmod.asm.hooks;
+
+import net.minecraft.client.entity.EntityOtherPlayerMP;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemSword;
+
+import java.util.Objects;
+
+@SuppressWarnings("unused")
+public class EntityOtherPlayerMPHook {
+
+    public static boolean shouldCancelEquipmentUpdate(EntityOtherPlayerMP player, int slotIn, ItemStack stack) {
+        if (slotIn == 0) {
+            final ItemStack currentStack = player.inventory.mainInventory[player.inventory.currentItem];
+            if (currentStack != null && stack != null && currentStack.getItem() instanceof ItemSword && areItemStacksSemiEquals(currentStack, stack)) {
+                currentStack.setItemDamage(stack.getItemDamage());
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean areItemStacksSemiEquals(ItemStack stackA, ItemStack stackB) {
+        return stackA.stackSize == stackB.stackSize && stackA.getItem() == stackB.getItem() && Objects.equals(stackA.getTagCompound(), stackB.getTagCompound());
+    }
+
+}
---
+++ src/main/java/fr/alexdoru/megawallsenhancementsmod/asm/transformers/EntityOtherPlayerMPTransformer_FixAutoblockBypass.java
@@ -0,0 +1,44 @@
+package fr.alexdoru.megawallsenhancementsmod.asm.transformers;
+
+import fr.alexdoru.megawallsenhancementsmod.asm.loader.InjectionStatus;
+import fr.alexdoru.megawallsenhancementsmod.asm.loader.MWETransformer;
+import fr.alexdoru.megawallsenhancementsmod.asm.mappings.ClassMapping;
+import fr.alexdoru.megawallsenhancementsmod.asm.mappings.MethodMapping;
+import org.objectweb.asm.tree.*;
+
+import static org.objectweb.asm.Opcodes.*;
+
+public class EntityOtherPlayerMPTransformer_FixAutoblockBypass implements MWETransformer {
+
+    @Override
+    public String[] getTargetClassName() {
+        return new String[]{"net.minecraft.client.entity.EntityOtherPlayerMP"};
+    }
+
+    @Override
+    public void transform(ClassNode classNode, InjectionStatus status) {
+        status.setInjectionPoints(1);
+        for (final MethodNode methodNode : classNode.methods) {
+            if (checkMethodNode(methodNode, MethodMapping.ENTITY$SETCURRENTITEMORARMOR)) {
+                final InsnList list = new InsnList();
+                list.add(new VarInsnNode(ALOAD, 0));
+                list.add(new VarInsnNode(ILOAD, 1));
+                list.add(new VarInsnNode(ALOAD, 2));
+                list.add(new MethodInsnNode(
+                        INVOKESTATIC,
+                        getHookClass("EntityOtherPlayerMPHook"),
+                        "shouldCancelEquipmentUpdate",
+                        "(L" + ClassMapping.ENTITYOTHERPLAYERMP + ";IL" + ClassMapping.ITEMSTACK + ";)Z",
+                        false
+                ));
+                final LabelNode notCancelled = new LabelNode();
+                list.add(new JumpInsnNode(IFEQ, notCancelled));
+                list.add(new InsnNode(RETURN));
+                list.add(notCancelled);
+                methodNode.instructions.insert(list);
+                status.addInjection();
+            }
+        }
+    }
+
+}
