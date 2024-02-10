f68dfe6abb4592cce5c283535bd87c58c2e859b1
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2024-02-10 22:29:48 +0100

    add debug command to logpackets received by the client

--- src/main/java/fr/alexdoru/megawallsenhancementsmod/asm/hooks/NetworkManagerHook_PacketListener.java
+++ src/main/java/fr/alexdoru/megawallsenhancementsmod/asm/hooks/NetworkManagerHook_PacketListener.java
@@ -3,19 +3,22 @@ package fr.alexdoru.megawallsenhancementsmod.asm.hooks;
 import fr.alexdoru.megawallsenhancementsmod.config.ConfigHandler;
 import fr.alexdoru.megawallsenhancementsmod.hackerdetector.AttackDetector;
 import fr.alexdoru.megawallsenhancementsmod.hackerdetector.debug.ClientPacketLogger;
+import fr.alexdoru.megawallsenhancementsmod.hackerdetector.debug.ServerPacketLogger;
 import net.minecraft.network.Packet;
 
 @SuppressWarnings("unused")
 public class NetworkManagerHook_PacketListener {
+
+    public static boolean logPackets = false;
 
     // Careful, this code isn't called from the main thread
     // Only listens packets that throw a ThreadQuickExitException
     public static void listen(Packet<?> packet) {
         if (!ConfigHandler.hackerDetector) return;
         try {
+            if (logPackets) ServerPacketLogger.logPacket(packet);
             AttackDetector.lookForAttacks(packet);
             //PacketHUD.INSTANCE.logPacket(packet);
-            //ServerPacketLogger.logPacket(packet);
         } catch (Throwable ignored) {}
     }
 
