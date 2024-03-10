9ba4c8d9594af4f402fe6971bc9b7dc0665e43d5
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2024-03-10 21:41:06 +0100

    WIP Keepsprint check

--- src/main/java/fr/alexdoru/megawallsenhancementsmod/hackerdetector/checks/KeepSprintACheck.java
+++ src/main/java/fr/alexdoru/megawallsenhancementsmod/hackerdetector/checks/KeepSprintACheck.java
@@ -7,7 +7,7 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
-public class KeepsprintCheck extends Check {
+public class KeepSprintACheck extends Check {
 
     @Override
     public String getCheatName() {
@@ -19,6 +19,11 @@ public class KeepsprintCheck extends Check {
         return "The player's sprint doesn't turn off when using items (blocking sword, eating, drinking, using bow...)";
     }
 
+    //@Override
+    //public String getFlagType() {
+    //    return "A";
+    //}
+
     @Override
     public boolean canSendReport() {
         return false;
@@ -26,7 +31,7 @@ public class KeepsprintCheck extends Check {
 
     @Override
     public void performCheck(EntityPlayer player, PlayerDataSamples data) {
-        super.checkViolationLevel(player, this.check(player, data), data.keepsprintVL);
+        super.checkViolationLevel(player, this.check(player, data), data.keepsprintAVL);
     }
 
     @Override
@@ -38,13 +43,13 @@ public class KeepsprintCheck extends Check {
             if (maxDuration == 0) return false; // wtf ??
             // it is possible to double eat -> start sprinting while eating a second item
             if (data.sprintTime > (maxDuration == 32 ? 70 : 5) || data.sprintTime > data.useItemTime + 5) {
-                data.keepsprintVL.add(2);
+                data.keepsprintAVL.add(2);
                 if (ConfigHandler.debugLogging) {
-                    this.log(player, data, data.keepsprintVL, null);
+                    this.log(player, data, data.keepsprintAVL, null);
                 }
                 return true;
             } else if (data.sprintTime == 0) {
-                data.keepsprintVL.substract(3);
+                data.keepsprintAVL.substract(3);
             }
         }
         return false;
@@ -54,7 +59,7 @@ public class KeepsprintCheck extends Check {
     protected void log(EntityPlayer player, PlayerDataSamples data, ViolationLevelTracker vl, String extramsg) {
         final ItemStack itemStack = player.getHeldItem();
         final Item item = itemStack == null ? null : itemStack.getItem();
-        super.log(player, data, data.keepsprintVL,
+        super.log(player, data, data.keepsprintAVL,
                 " | sprintTime " + data.sprintTime
                         + " | useItemTime " + data.useItemTime
                         + " | speedXZ " + String.format("%.2f", data.getSpeedXZ())
