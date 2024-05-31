888076fce0c3fb4c5df237043f06a65c9c48d848
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2024-05-31 21:24:33 +0200

    minor config menu text changes
    (cherry picked from commit b91ba9dfb081cf88b833cd86d574e0fb4394f93f)

--- src/main/java/fr/alexdoru/megawallsenhancementsmod/gui/guiscreens/HackerDetectorConfigGuiScreen.java
+++ src/main/java/fr/alexdoru/megawallsenhancementsmod/gui/guiscreens/HackerDetectorConfigGuiScreen.java
@@ -42,7 +42,7 @@ public class HackerDetectorConfigGuiScreen extends MyGuiScreen {
                 GRAY + "Saves flagged players in NoCheaters"));
         this.buttonList.add(new OptionGuiButton(
                 xPos, getButtonYPos(4),
-                "Report Flagged players",
+                "Auto-report cheaters",
                 (b) -> {
                     ConfigHandler.autoreportFlaggedPlayers = b;
                     if (!ConfigHandler.autoreportFlaggedPlayers) {
@@ -50,8 +50,8 @@ public class HackerDetectorConfigGuiScreen extends MyGuiScreen {
                     }
                 },
                 () -> ConfigHandler.autoreportFlaggedPlayers,
-                GRAY + "Sends a report automatically to Hypixel when it flags a cheater",
-                YELLOW + "Only works in Mega Walls, sends one report per game per player. It will abort the report if you wait too long to send it."));
+                GRAY + "Sends a /report automatically to Hypixel when it flags a cheater",
+                YELLOW + "Only works in Mega Walls, sends one report per game per player. It will abort the report after 30 seconds if you wait too long to send it."));
         new HUDSettingGuiButtons(
                 getxCenter(), getButtonYPos(5),
                 () -> {
