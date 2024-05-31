29a8e4a20914426125f2081c79dae8f859dfa964
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2024-05-31 22:09:03 +0200

    don't type auto-report if the player is currently typing in chat
    (cherry picked from commit 3b1ca652afe6290247870d7d5e79399cfff3808f)

--- src/main/java/fr/alexdoru/megawallsenhancementsmod/asm/accessors/GuiChatAccessor.java
+++ src/main/java/fr/alexdoru/megawallsenhancementsmod/asm/accessors/GuiChatAccessor.java
@@ -1,9 +1,13 @@
 package fr.alexdoru.megawallsenhancementsmod.asm.accessors;
 
+import net.minecraft.client.gui.GuiTextField;
+
 public interface GuiChatAccessor {
 
     int getSentHistoryCursor();
 
     void setSentHistoryCursor(int i);
+
+    GuiTextField getInputField();
 
 }
