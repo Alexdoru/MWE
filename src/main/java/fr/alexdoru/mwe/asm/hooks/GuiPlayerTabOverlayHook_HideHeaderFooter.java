8d076d6e0726b145c33aeb647806f572efe7b72d
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2024-07-25 01:37:30 +0200

    fix drawing background rect of the tablist header when there is no header to render

--- src/main/java/fr/alexdoru/mwe/asm/hooks/GuiPlayerTabOverlayHook_HideHeaderFooter.java
+++ src/main/java/fr/alexdoru/mwe/asm/hooks/GuiPlayerTabOverlayHook_HideHeaderFooter.java
@@ -2,12 +2,13 @@ package fr.alexdoru.mwe.asm.hooks;
 
 import fr.alexdoru.mwe.config.ConfigHandler;
 import fr.alexdoru.mwe.scoreboard.ScoreboardTracker;
+import net.minecraft.client.Minecraft;
 
 @SuppressWarnings("unused")
 public class GuiPlayerTabOverlayHook_HideHeaderFooter {
 
     public static boolean shouldRenderHeader() {
-        return ConfigHandler.showPlayercountTablist || shouldRenderHeaderFooter();
+        return ConfigHandler.showPlayercountTablist && Minecraft.getMinecraft().thePlayer.sendQueue.getPlayerInfoMap().size() > 1 || shouldRenderHeaderFooter();
     }
 
     public static boolean shouldRenderHeaderFooter() {
