ab5ea8abe4a7098d6c23ced782e758c255047994
William <32989615+William0216@users.noreply.github.com>
2025-02-20 18:41:49 -0500

    Update CommandMWE.java

--- src/main/java/fr/alexdoru/mwe/commands/CommandMWE.java
+++ src/main/java/fr/alexdoru/mwe/commands/CommandMWE.java
@@ -28,11 +28,11 @@ public class CommandMWE extends MyAbstractCommand {
             }
         } else if (args.length >= 1 && (args[0].equalsIgnoreCase("howplaygame") || args[0].equalsIgnoreCase("hpg") || args[0].equalsIgnoreCase("explain"))) {
             if (ScoreboardTracker.isMWEnvironement()) {
-                final String msg1 = "Before the walls fall, mine iron to make armor and store it + food in your enderchest";
-                final String msg2 = "When the walls fall, rush mid to fight for the initial diamonds";
-                final String msg3 = "Each team has a wither, protect yours and kill enemy withers. Wither death = No Respawns";
-                final String msg4 = "To win, Be the last team standing or cause the most player damage during Deathmatch (the last phase)";
-                final String msg5 = "You can upgrade your starting armor or change classes for different abilities in the lobby";
+                final String msg1 = "Before the walls fall, mine iron to make armor and store it + food in your enderchest.";
+                final String msg2 = "When the walls fall, rush mid to fight for the initial diamonds. Each team has a wither.";
+                final String msg3 = "Protect yours and kill enemy withers to stop player respawns. Win by being the last team alive or by";
+                final String msg4 = "causing the most player damage during Deathmatch (the last phase). Upgrade your kit in the lobby.";
+                final String msg5 = "Outdated but more detailed guide: https://hypixel.net/threads/the-complete-mega-walls-guide.3489088/";
                 sendChatMessage(msg1);
                 new DelayedTask(() -> sendChatMessage(msg2), 80);
                 new DelayedTask(() -> sendChatMessage(msg3), 155);
