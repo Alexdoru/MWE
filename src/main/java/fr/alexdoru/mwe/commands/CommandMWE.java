6f6966474327aa3ea86b9b2baf11782738286400
William <32989615+William0216@users.noreply.github.com>
2025-07-07 03:03:12 -0400

    "echest" > "ender chest"; msg5 updated; "+" to "and"
    Requested msg5 change doesn't fit in Minecraft chat. Had to rephrase.

--- src/main/java/fr/alexdoru/mwe/commands/CommandMWE.java
+++ src/main/java/fr/alexdoru/mwe/commands/CommandMWE.java
@@ -28,11 +28,11 @@ public class CommandMWE extends MyAbstractCommand {
             }
         } else if (args.length >= 1 && (args[0].equalsIgnoreCase("howplaygame") || args[0].equalsIgnoreCase("hpg") || args[0].equalsIgnoreCase("explain"))) {
             if (ScoreboardTracker.isMWEnvironement()) {
-                final String msg1 = "At the start mine iron to make armor. Store armor + food in echest.";
+                final String msg1 = "At the start mine iron to make armor. Store armor and food in your ender chest.";
                 final String msg2 = "When the walls fall, rush mid for diamonds then attack enemy withers or defend yours.";
                 final String msg3 = "When your wither dies, you can't respawn so meet team at spawn and stay together.";
                 final String msg4 = "Win by causing the most damage after all withers die or by being the last team alive.";
-                final String msg5 = "You can share resources with your team using /tc and upgrade your kit in the lobby.";
+                final String msg5 = "You can share resources with your team using /tc. You can upgrade your kit at the shop in the lobby.";
                 sendChatMessage(msg1);
                 new DelayedTask(() -> sendChatMessage(msg2), 80);
                 new DelayedTask(() -> sendChatMessage(msg3), 155);
