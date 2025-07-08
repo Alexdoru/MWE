db1c6cd1774c0f13c9ef176dfa8590397f51ee41
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2025-07-08 10:08:06 +0200

    update howplaygame text

--- src/main/java/fr/alexdoru/mwe/commands/CommandMWE.java
+++ src/main/java/fr/alexdoru/mwe/commands/CommandMWE.java
@@ -28,11 +28,11 @@ public class CommandMWE extends MyAbstractCommand {
             }
         } else if (args.length >= 1 && (args[0].equalsIgnoreCase("howplaygame") || args[0].equalsIgnoreCase("hpg") || args[0].equalsIgnoreCase("explain"))) {
             if (ScoreboardTracker.isMWEnvironement()) {
-                final String msg1 = "At the start, mine iron to make armor. Store armor and food in your ender chest.";
+                final String msg1 = "Before the walls fall, mine iron to make armor. Store armor, blocks and food in your ender chest.";
                 final String msg2 = "When the walls fall, rush mid for diamonds then attack enemy withers or defend yours.";
-                final String msg3 = "When your wither dies, you can't respawn so meet your team at spawn and stay together.";
-                final String msg4 = "Win by causing the most damage after all withers die or by being the last team alive.";
-                final String msg5 = "You can share resources with your team using /tc. Your kit can be upgraded at the shop in the lobby.";
+                final String msg3 = "After your wither dies, you can't respawn, so meet your team at spawn and stay together.";
+                final String msg4 = "Win by being the last team standing, or the team with most damage during deathmatch.";
+                final String msg5 = "You can share resources with your team using /tc. Upgrade your kit using the shop in the lobby.";
                 sendChatMessage(msg1);
                 new DelayedTask(() -> sendChatMessage(msg2), 80);
                 new DelayedTask(() -> sendChatMessage(msg3), 155);
