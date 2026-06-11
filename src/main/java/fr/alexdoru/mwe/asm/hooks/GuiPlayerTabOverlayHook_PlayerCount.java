d125e0d20792beb063603ca3fd2245e1ed81b6e0
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2026-06-12 01:27:50 +0200

    show amount of fake players at top of the tablist

--- src/main/java/fr/alexdoru/mwe/asm/hooks/GuiPlayerTabOverlayHook_PlayerCount.java
+++ src/main/java/fr/alexdoru/mwe/asm/hooks/GuiPlayerTabOverlayHook_PlayerCount.java
@@ -1,14 +1,16 @@
 package fr.alexdoru.mwe.asm.hooks;
 
 import fr.alexdoru.mwe.config.MWEConfig;
+import fr.alexdoru.mwe.utils.NameUtil;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.network.NetworkPlayerInfo;
-import net.minecraft.util.EnumChatFormatting;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+
+import static net.minecraft.util.EnumChatFormatting.*;
 
 public class GuiPlayerTabOverlayHook_PlayerCount {
 
@@ -23,7 +25,17 @@ public class GuiPlayerTabOverlayHook_PlayerCount {
         if (i < 2) {
             return GuiPlayerTabOverlayHook_HideHeaderFooter.shouldRenderHeaderFooter() ? listIn : list;
         }
-        list.add(EnumChatFormatting.GREEN + "Players: " + EnumChatFormatting.GOLD + i);
+        if (MWEConfig.showFakePlayersInTab) {
+            int nicks = 0;
+            for (final NetworkPlayerInfo netInfo : playerInfoMap) {
+                if (NameUtil.isNickedPlayer(netInfo.getGameProfile().getId())) {
+                    nicks++;
+                }
+            }
+            list.add(GREEN + "Players: " + GOLD + i + (nicks == 0 ? "" : WHITE + " (" + GOLD + nicks + WHITE + ")"));
+        } else {
+            list.add(GREEN + "Players: " + GOLD + i);
+        }
         if (GuiPlayerTabOverlayHook_HideHeaderFooter.shouldRenderHeaderFooter()) {
             list.addAll(listIn);
         }
