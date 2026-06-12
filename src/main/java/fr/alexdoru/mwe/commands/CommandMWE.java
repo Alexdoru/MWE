6199098c4d076709ea9f5eb397a305480a810c30
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2026-06-12 10:52:32 +0200

    add cooldown on mwe howplaygame command

--- src/main/java/fr/alexdoru/mwe/commands/CommandMWE.java
+++ src/main/java/fr/alexdoru/mwe/commands/CommandMWE.java
@@ -5,6 +5,7 @@ import fr.alexdoru.mwe.config.lib.ConfigHandler;
 import fr.alexdoru.mwe.http.apikey.HypixelApiKeyUtil;
 import fr.alexdoru.mwe.scoreboard.ScoreboardTracker;
 import fr.alexdoru.mwe.utils.DelayedTask;
+import fr.alexdoru.mwe.utils.TimerUtil;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumChatFormatting;
@@ -12,6 +13,8 @@ import net.minecraft.util.EnumChatFormatting;
 import java.util.List;
 
 public class CommandMWE extends MyAbstractCommand {
+
+    private final TimerUtil timer = new TimerUtil(5 * 60 * 1000);
 
     @Override
     public String getCommandName() {
@@ -28,16 +31,20 @@ public class CommandMWE extends MyAbstractCommand {
             }
         } else if (args.length >= 1 && (args[0].equalsIgnoreCase("howplaygame") || args[0].equalsIgnoreCase("hpg") || args[0].equalsIgnoreCase("explain"))) {
             if (ScoreboardTracker.isMWEnvironement()) {
-                final String msg1 = "Before the walls fall, mine iron to make armor. Store armor, blocks and food in your ender chest.";
-                final String msg2 = "When the walls fall, rush mid for diamonds then attack enemy withers or defend yours.";
-                final String msg3 = "After your wither dies, you can't respawn, so meet your team at spawn and stay together.";
-                final String msg4 = "Win by being the last team standing, or the team with most damage during deathmatch.";
-                final String msg5 = "You can share resources with your team using /tc. Upgrade your kit using the shop in the lobby.";
-                sendChatMessage(msg1);
-                new DelayedTask(() -> sendChatMessage(msg2), 80);
-                new DelayedTask(() -> sendChatMessage(msg3), 155);
-                new DelayedTask(() -> sendChatMessage(msg4), 240);
-                new DelayedTask(() -> sendChatMessage(msg5), 320);
+                if (this.timer.update()) {
+                    final String msg1 = "Before the walls fall, mine iron to make armor. Store armor, blocks and food in your ender chest.";
+                    final String msg2 = "When the walls fall, rush mid for diamonds then attack enemy withers or defend yours.";
+                    final String msg3 = "After your wither dies, you can't respawn, so meet your team at spawn and stay together.";
+                    final String msg4 = "Win by being the last team standing, or the team with most damage during deathmatch.";
+                    final String msg5 = "You can share resources with your team using /tc. Upgrade your kit using the shop in the lobby.";
+                    sendChatMessage(msg1);
+                    new DelayedTask(() -> sendChatMessage(msg2), 80);
+                    new DelayedTask(() -> sendChatMessage(msg3), 155);
+                    new DelayedTask(() -> sendChatMessage(msg4), 240);
+                    new DelayedTask(() -> sendChatMessage(msg5), 320);
+                } else {
+                    ChatUtil.addChatMessage(ChatUtil.getTagMW() + EnumChatFormatting.RED + "Command is on cooldown");
+                }
             } else {
                 ChatUtil.addChatMessage(ChatUtil.getTagMW() + EnumChatFormatting.RED + "Command only works in Mega Walls");
             }
