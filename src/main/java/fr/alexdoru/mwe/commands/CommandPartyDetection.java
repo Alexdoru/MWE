407864675a8615aeefd70386eb1fe87b693029c7
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2026-06-26 15:33:47 +0200

    hide disconnected parties in CommandPartyDetection

--- src/main/java/fr/alexdoru/mwe/commands/CommandPartyDetection.java
+++ src/main/java/fr/alexdoru/mwe/commands/CommandPartyDetection.java
@@ -26,8 +26,7 @@ public class CommandPartyDetection extends MyAbstractCommand {
             return;
         }
 
-        ChatUtil.addChatMessage(EnumChatFormatting.GREEN + ChatUtil.bar());
-        ChatUtil.addChatMessage(ChatUtil.centerLine(EnumChatFormatting.GREEN + "Partys in lobby " + EnumChatFormatting.YELLOW + PartyDetection.getServerID()));
+        boolean isFirst = true;
 
         final Set<String> playerPrinted = new HashSet<>();
         for (final String player : PartyDetection.getPlayersWithParty()) {
@@ -35,19 +34,34 @@ public class CommandPartyDetection extends MyAbstractCommand {
             final Set<String> party = PartyDetection.getPartyOf(player);
             playerPrinted.addAll(party);
             final StringBuilder sb = new StringBuilder();
+            boolean flag = false;
             sb.append(EnumChatFormatting.DARK_GRAY).append("- ");
             for (final String playername : party) {
                 final NetworkPlayerInfo netInfo = NetPlayerInfoTracker.getPlayerInfo(playername);
                 if (netInfo == null) {
                     sb.append(EnumChatFormatting.DARK_GRAY).append(playername).append(" ");
+                    continue;
                 } else {
-                    sb.append(EnumChatFormatting.RESET).append(NameFormatter.getFormattedName(netInfo)).append(" ");
+                    flag = true;
                 }
+                sb.append(EnumChatFormatting.DARK_GRAY).append(NameFormatter.getFormattedName(netInfo)).append(EnumChatFormatting.RESET).append(" ");
             }
-            ChatUtil.addChatMessage(sb.toString());
+            if (flag) {
+                if (isFirst) {
+                    ChatUtil.addChatMessage(EnumChatFormatting.GREEN + ChatUtil.bar());
+                    ChatUtil.addChatMessage(ChatUtil.centerLine(EnumChatFormatting.GREEN + "Partys in lobby " + EnumChatFormatting.YELLOW + PartyDetection.getServerID()));
+                    isFirst = false;
+                }
+                ChatUtil.addChatMessage("");
+                ChatUtil.addChatMessage(sb.toString());
+            }
         }
 
-        ChatUtil.addChatMessage(EnumChatFormatting.GREEN + ChatUtil.bar());
+        if (isFirst) {
+            ChatUtil.addChatMessage(EnumChatFormatting.RED + "No party in current lobby.");
+        } else {
+            ChatUtil.addChatMessage(EnumChatFormatting.GREEN + ChatUtil.bar());
+        }
     }
 
 }
