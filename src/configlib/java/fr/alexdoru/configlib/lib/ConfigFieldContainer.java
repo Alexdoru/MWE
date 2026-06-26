34e350a8b9853ba2d426b381d782898d51aa68b6
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2026-06-26 16:16:43 +0200

    config : create FieldType enum in ConfigFieldContainer

--- src/configlib/java/fr/alexdoru/configlib/lib/ConfigFieldContainer.java
+++ src/configlib/java/fr/alexdoru/configlib/lib/ConfigFieldContainer.java
@@ -23,6 +23,7 @@ public final class ConfigFieldContainer {
 
     private final Map<String, Property> propertyMap;
     private final Field field;
+    private final FieldType fieldType;
     private final ConfigProperty annotation;
     private final Method event;
     private final Method hideOverride;
@@ -35,6 +36,7 @@ public final class ConfigFieldContainer {
             Map<String, Method> configHideOverrides) throws IllegalAccessException {
         this.propertyMap = propertyMap;
         this.field = field;
+        this.fieldType = getFieldType(field);
         this.annotation = field.getAnnotation(ConfigProperty.class);
         if (this.propertyMap.containsKey(annotation.name())) {
             throw new IllegalStateException("Duplicate key names in config properties : " + annotation.name());
@@ -45,92 +47,160 @@ public final class ConfigFieldContainer {
         this.loadConfigValueToField();
     }
 
-    private void createPropertyFromField(Configuration config) throws IllegalAccessException {
+    private static FieldType getFieldType(Field field) {
         if (field.getType() == RendererPosition.class) {
-            final String showKey = "Show " + annotation.name();
-            final String xKey = "Xpos " + annotation.name();
-            final String yKey = "Ypos " + annotation.name();
-            final RendererPosition rendererPosition = (RendererPosition) field.get(null);
-            propertyMap.put(showKey, config.get(annotation.category(), showKey, rendererPosition.isEnabled()));
-            propertyMap.put(xKey, config.get(annotation.category(), xKey, rendererPosition.getRelativeX()));
-            propertyMap.put(yKey, config.get(annotation.category(), yKey, rendererPosition.getRelativeY()));
+            return FieldType.RENDERER;
         } else if (field.getType() == String.class) {
-            propertyMap.put(annotation.name(), config.get(annotation.category(), annotation.name(), (String) field.get(null)));
+            return FieldType.STRING;
         } else if (field.getType() == boolean.class) {
-            propertyMap.put(annotation.name(), config.get(annotation.category(), annotation.name(), (boolean) field.get(null)));
+            return FieldType.BOOLEAN;
         } else if (field.getType() == double.class) {
-            propertyMap.put(annotation.name(), config.get(annotation.category(), annotation.name(), (double) field.get(null)));
+            return FieldType.DOUBLE;
         } else if (field.getType() == int.class) {
-            propertyMap.put(annotation.name(), config.get(annotation.category(), annotation.name(), (int) field.get(null)));
+            return FieldType.INT;
         } else if (field.getType() == EnumChatFormatting.class) {
-            final EnumChatFormatting color = (EnumChatFormatting) field.get(null);
-            Objects.requireNonNull(color);
-            if (!color.isColor()) {
-                throw new IllegalArgumentException("EnumChatFormatting fields must be colors!");
-            }
-            propertyMap.put(annotation.name(), config.get(annotation.category(), annotation.name(), color.name()));
+            return FieldType.ENUM_COLOR;
         } else if (field.getGenericType().toString().equals("java.util.List<java.lang.String>")) {
-            //noinspection unchecked
-            final String[] defaultStrings = ((List<String>) field.get(null)).toArray(new String[0]);
-            propertyMap.put(annotation.name(), config.get(annotation.category(), annotation.name(), defaultStrings));
+            return FieldType.STRING_LIST;
         } else {
             throw new IllegalArgumentException("Type of field not handled by config lib " + field.getType());
+        }
+    }
+
+    private void createPropertyFromField(Configuration config) throws IllegalAccessException {
+        switch (this.fieldType) {
+            case RENDERER: {
+                final String showKey = "Show " + annotation.name();
+                final String xKey = "Xpos " + annotation.name();
+                final String yKey = "Ypos " + annotation.name();
+                final RendererPosition rendererPosition = (RendererPosition) field.get(null);
+                propertyMap.put(showKey, config.get(annotation.category(), showKey, rendererPosition.isEnabled()));
+                propertyMap.put(xKey, config.get(annotation.category(), xKey, rendererPosition.getRelativeX()));
+                propertyMap.put(yKey, config.get(annotation.category(), yKey, rendererPosition.getRelativeY()));
+                break;
+            }
+            case STRING: {
+                propertyMap.put(annotation.name(), config.get(annotation.category(), annotation.name(), (String) field.get(null)));
+                break;
+            }
+            case BOOLEAN: {
+                propertyMap.put(annotation.name(), config.get(annotation.category(), annotation.name(), (boolean) field.get(null)));
+                break;
+            }
+            case DOUBLE: {
+                propertyMap.put(annotation.name(), config.get(annotation.category(), annotation.name(), (double) field.get(null)));
+                break;
+            }
+            case INT: {
+                propertyMap.put(annotation.name(), config.get(annotation.category(), annotation.name(), (int) field.get(null)));
+                break;
+            }
+            case ENUM_COLOR: {
+                final EnumChatFormatting color = (EnumChatFormatting) field.get(null);
+                Objects.requireNonNull(color);
+                if (!color.isColor()) {
+                    throw new IllegalArgumentException("EnumChatFormatting fields must be colors!");
+                }
+                propertyMap.put(annotation.name(), config.get(annotation.category(), annotation.name(), color.name()));
+                break;
+            }
+            case STRING_LIST: {
+                //noinspection unchecked
+                final String[] defaultStrings = ((List<String>) field.get(null)).toArray(new String[0]);
+                propertyMap.put(annotation.name(), config.get(annotation.category(), annotation.name(), defaultStrings));
+                break;
+            }
+            default: {
+                throw new IllegalArgumentException("Type of field not handled by config lib " + field.getType());
+            }
         }
     }
 
     private void loadConfigValueToField() throws IllegalAccessException {
-        if (field.getType() == RendererPosition.class) {
-            final RendererPosition position = ((RendererPosition) field.get(null));
-            position.setEnabled(propertyMap.get("Show " + annotation.name()).getBoolean());
-            position.setRelativePosition(
-                    propertyMap.get("Xpos " + annotation.name()).getDouble(),
-                    propertyMap.get("Ypos " + annotation.name()).getDouble());
-        } else if (field.getType() == String.class) {
-            field.set(null, propertyMap.get(annotation.name()).getString());
-        } else if (field.getType() == boolean.class) {
-            field.setBoolean(null, propertyMap.get(annotation.name()).getBoolean());
-        } else if (field.getType() == double.class) {
-            field.setDouble(null, propertyMap.get(annotation.name()).getDouble());
-        } else if (field.getType() == int.class) {
-            field.setInt(null, propertyMap.get(annotation.name()).getInt());
-        } else if (field.getType() == EnumChatFormatting.class) {
-            EnumChatFormatting color = getColorFromString(propertyMap.get(annotation.name()).toString());
-            if (color == null) {
-                color = getColorFromString(propertyMap.get(annotation.name()).getDefault());
+        switch (this.fieldType) {
+            case RENDERER: {
+                final RendererPosition position = ((RendererPosition) field.get(null));
+                position.setEnabled(propertyMap.get("Show " + annotation.name()).getBoolean());
+                position.setRelativePosition(
+                        propertyMap.get("Xpos " + annotation.name()).getDouble(),
+                        propertyMap.get("Ypos " + annotation.name()).getDouble());
+                break;
             }
-            field.set(null, color);
-        } else if (field.getGenericType().toString().equals("java.util.List<java.lang.String>")) {
-            final String[] strings = propertyMap.get(annotation.name()).getStringList();
-            //noinspection unchecked
-            final List<String> list = (List<String>) field.get(null);
-            list.clear();
-            list.addAll(Arrays.asList(strings));
-        } else {
-            throw new IllegalArgumentException("Type of field not handled by config lib " + field.getType());
+            case STRING: {
+                field.set(null, propertyMap.get(annotation.name()).getString());
+                break;
+            }
+            case BOOLEAN: {
+                field.setBoolean(null, propertyMap.get(annotation.name()).getBoolean());
+                break;
+            }
+            case DOUBLE: {
+                field.setDouble(null, propertyMap.get(annotation.name()).getDouble());
+                break;
+            }
+            case INT: {
+                field.setInt(null, propertyMap.get(annotation.name()).getInt());
+                break;
+            }
+            case ENUM_COLOR: {
+                EnumChatFormatting color = getColorFromString(propertyMap.get(annotation.name()).toString());
+                if (color == null) {
+                    color = getColorFromString(propertyMap.get(annotation.name()).getDefault());
+                }
+                field.set(null, color);
+                break;
+            }
+            case STRING_LIST: {
+                final String[] strings = propertyMap.get(annotation.name()).getStringList();
+                //noinspection unchecked
+                final List<String> list = (List<String>) field.get(null);
+                list.clear();
+                list.addAll(Arrays.asList(strings));
+                break;
+            }
+            default: {
+                throw new IllegalArgumentException("Type of field not handled by config lib " + field.getType());
+            }
         }
     }
 
     void saveFieldValueToConfig() throws IllegalAccessException {
-        if (field.getType() == RendererPosition.class) {
-            final RendererPosition rendererPosition = (RendererPosition) field.get(null);
-            propertyMap.get("Show " + annotation.name()).set(rendererPosition.isEnabled());
-            propertyMap.get("Xpos " + annotation.name()).set(rendererPosition.getRelativeX());
-            propertyMap.get("Ypos " + annotation.name()).set(rendererPosition.getRelativeY());
-        } else if (field.getType() == String.class) {
-            propertyMap.get(annotation.name()).set((String) field.get(null));
-        } else if (field.getType() == boolean.class) {
-            propertyMap.get(annotation.name()).set((boolean) field.get(null));
-        } else if (field.getType() == double.class) {
-            propertyMap.get(annotation.name()).set((double) field.get(null));
-        } else if (field.getType() == int.class) {
-            propertyMap.get(annotation.name()).set((int) field.get(null));
-        } else if (field.getType() == EnumChatFormatting.class) {
-            propertyMap.get(annotation.name()).set(((EnumChatFormatting) field.get(null)).name());
-        } else if (field.getGenericType().toString().equals("java.util.List<java.lang.String>")) {
-            //noinspection unchecked
-            propertyMap.get(annotation.name()).set(((List<String>) field.get(null)).toArray(new String[0]));
-        } else {
-            throw new IllegalArgumentException("Type of field not handled by config lib " + field.getType());
+        switch (this.fieldType) {
+            case RENDERER: {
+                final RendererPosition rendererPosition = (RendererPosition) field.get(null);
+                propertyMap.get("Show " + annotation.name()).set(rendererPosition.isEnabled());
+                propertyMap.get("Xpos " + annotation.name()).set(rendererPosition.getRelativeX());
+                propertyMap.get("Ypos " + annotation.name()).set(rendererPosition.getRelativeY());
+                break;
+            }
+            case STRING: {
+                propertyMap.get(annotation.name()).set((String) field.get(null));
+                break;
+            }
+            case BOOLEAN: {
+                propertyMap.get(annotation.name()).set((boolean) field.get(null));
+                break;
+            }
+            case DOUBLE: {
+                propertyMap.get(annotation.name()).set((double) field.get(null));
+                break;
+            }
+            case INT: {
+                propertyMap.get(annotation.name()).set((int) field.get(null));
+                break;
+            }
+            case ENUM_COLOR: {
+                propertyMap.get(annotation.name()).set(((EnumChatFormatting) field.get(null)).name());
+                break;
+            }
+            case STRING_LIST: {
+                //noinspection unchecked
+                propertyMap.get(annotation.name()).set(((List<String>) field.get(null)).toArray(new String[0]));
+                break;
+            }
+            default: {
+                throw new IllegalArgumentException("Type of field not handled by config lib " + field.getType());
+            }
         }
     }
 
@@ -142,27 +212,33 @@ public final class ConfigFieldContainer {
                 if (shouldHide) return null;
             } catch (InvocationTargetException ignored) {}
         }
-        if (field.getType() == RendererPosition.class) {
-            return new RendererGuiButton(configGuiScreen, rendererManager, field, event, annotation);
-        } else if (field.getType() == boolean.class) {
-            return new BooleanGuiButton(field, event, annotation);
-        } else if (field.getType() == double.class) {
-            if (annotation.sliderMin() == annotation.sliderMax()) {
-                throw new IllegalArgumentException("Config slider cannot have same min and max values. Name : " + annotation.name());
+        switch (this.fieldType) {
+            case RENDERER: {
+                return new RendererGuiButton(configGuiScreen, rendererManager, field, event, annotation);
             }
-            return new SliderGuiButton(field, event, annotation);
-        } else if (field.getType() == int.class) {
-            if (annotation.isColor()) {
-                final int defaultColor = Integer.parseInt(propertyMap.get(annotation.name()).getDefault());
-                return new ColorGuiButton(configGuiScreen, field, annotation, defaultColor);
-            } else {
+            case BOOLEAN: {
+                return new BooleanGuiButton(field, event, annotation);
+            }
+            case DOUBLE: {
                 if (annotation.sliderMin() == annotation.sliderMax()) {
                     throw new IllegalArgumentException("Config slider cannot have same min and max values. Name : " + annotation.name());
                 }
                 return new SliderGuiButton(field, event, annotation);
             }
-        } else if (field.getType() == EnumChatFormatting.class) {
-            return new ColorEnumGuiButton(field, event, annotation);
+            case INT: {
+                if (annotation.isColor()) {
+                    final int defaultColor = Integer.parseInt(propertyMap.get(annotation.name()).getDefault());
+                    return new ColorGuiButton(configGuiScreen, field, annotation, defaultColor);
+                } else {
+                    if (annotation.sliderMin() == annotation.sliderMax()) {
+                        throw new IllegalArgumentException("Config slider cannot have same min and max values. Name : " + annotation.name());
+                    }
+                    return new SliderGuiButton(field, event, annotation);
+                }
+            }
+            case ENUM_COLOR: {
+                return new ColorEnumGuiButton(field, event, annotation);
+            }
         }
         throw new IllegalArgumentException("Type of field not handled by config lib gui screen " + field.getType() + " you can mark the field as hidden to prevent crashing");
     }
@@ -178,6 +254,16 @@ public final class ConfigFieldContainer {
             if (chatColor.isColor()) return chatColor;
         } catch (IllegalArgumentException ignored) {}
         return null;
+    }
+
+    enum FieldType {
+        RENDERER,
+        STRING,
+        BOOLEAN,
+        DOUBLE,
+        INT,
+        ENUM_COLOR,
+        STRING_LIST
     }
 
 }
