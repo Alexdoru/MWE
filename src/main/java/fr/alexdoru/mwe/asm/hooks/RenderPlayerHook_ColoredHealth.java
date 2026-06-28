550952bf143d916f7e221b5c5da412635d5a680f
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2026-06-28 17:53:13 +0200

    fix : use max health for score above head

--- src/main/java/fr/alexdoru/mwe/asm/hooks/RenderPlayerHook_ColoredHealth.java
+++ src/main/java/fr/alexdoru/mwe/asm/hooks/RenderPlayerHook_ColoredHealth.java
@@ -1,7 +1,6 @@
 package fr.alexdoru.mwe.asm.hooks;
 
 import fr.alexdoru.mwe.config.MWEConfig;
-import fr.alexdoru.mwe.scoreboard.ScoreboardTracker;
 import fr.alexdoru.mwe.utils.ColorUtil;
 import net.minecraft.client.entity.AbstractClientPlayer;
 
@@ -9,11 +8,7 @@ public class RenderPlayerHook_ColoredHealth {
 
     public static StringBuilder getColoredScore(StringBuilder str, int score, AbstractClientPlayer entity) {
         if (MWEConfig.coloredScoreAboveHead) {
-            if (ScoreboardTracker.isInMwGame()) {
-                return str.append(ColorUtil.getHPColor(44, score)).append(score);
-            } else {
-                return str.append(ColorUtil.getHPColor(entity.getMaxHealth(), score)).append(score);
-            }
+            return str.append(ColorUtil.getHPColor(entity.getMaxHealth(), score)).append(score);
         }
         return str.append(score);
     }
