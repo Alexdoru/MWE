2e88587a3ea9d7c9fe94be207c251202674acea9
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2026-07-18 17:14:30 +0200

    api : add MWSkin enum

--- src/main/java/fr/alexdoru/mwe/chat/SkinChatHead.java
+++ src/main/java/fr/alexdoru/mwe/chat/SkinChatHead.java
@@ -1,10 +1,15 @@
 package fr.alexdoru.mwe.chat;
 
+import net.minecraft.client.resources.DefaultPlayerSkin;
 import net.minecraft.util.ResourceLocation;
 
 public class SkinChatHead {
 
     private ResourceLocation skin;
+
+    public SkinChatHead() {
+        this.skin = DefaultPlayerSkin.getDefaultSkinLegacy();
+    }
 
     public SkinChatHead(ResourceLocation skin) {
         this.skin = skin;
