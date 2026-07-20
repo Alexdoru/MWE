1523774e71d83fde01cc6da04209a352fef384f3
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2026-07-20 15:49:56 +0200

    add failing test

---
+++ src/test/java/fr/alexdoru/mwe/utils/StringUtilTest.java
@@ -0,0 +1,14 @@
+package fr.alexdoru.mwe.utils;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class StringUtilTest {
+
+    @Test
+    public void testtt() {
+        assertEquals(1, 2);
+    }
+
+}
