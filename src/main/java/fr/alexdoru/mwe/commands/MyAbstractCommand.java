fbd94c3320f8ad386d7d0ac3f846b77e7b024370
shvaich <260186867+shvaich@users.noreply.github.com>
2026-07-22 21:12:11 +0300

    Commands help edit (#80)
    Co-authored-by: Alexdoru <57050655+Alexdoru@users.noreply.github.com>

--- src/main/java/fr/alexdoru/mwe/commands/MyAbstractCommand.java
+++ src/main/java/fr/alexdoru/mwe/commands/MyAbstractCommand.java
@@ -1,8 +1,14 @@
 package fr.alexdoru.mwe.commands;
 
+import fr.alexdoru.mwe.chat.ChatUtil;
 import net.minecraft.client.Minecraft;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommandSender;
+import net.minecraft.event.ClickEvent;
+import net.minecraft.event.HoverEvent;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.ChatStyle;
+import net.minecraft.util.EnumChatFormatting;
 
 public abstract class MyAbstractCommand extends CommandBase {
 
@@ -33,4 +39,38 @@ public abstract class MyAbstractCommand extends CommandBase {
         }
     }
 
+    protected static boolean isHelpSubcommand(String subcommand) {
+        return "h".equalsIgnoreCase(subcommand) || "help".equalsIgnoreCase(subcommand);
+    }
+
+    /**
+     * Prints command help lines grouped inside 2 horizontal bars
+     *
+     * @see #printCommandHelpBlock(String, String[][])
+     */
+    protected static void printCommandHelpBlock(EnumChatFormatting barColor, String header, String[][] commandLines) {
+        final String bar = barColor + ChatUtil.bar();
+        ChatUtil.addChatMessage(new ChatComponentText(bar));
+        MyAbstractCommand.printCommandHelpBlock(header, commandLines);
+        ChatUtil.addChatMessage(new ChatComponentText(bar));
+    }
+
+    /**
+     * @param header Block Header
+     * @param commandLines Each command-line must be in this format: { command, description, [commandToPutOnClick] }.
+     *                     if (commandToPutOnClick) isn't provided will use (command)
+     */
+    private static void printCommandHelpBlock(String header, String[][] commandLines) {
+        ChatUtil.addChatMessage(new ChatComponentText(ChatUtil.centerLine(EnumChatFormatting.GOLD + header)));
+        for (final String[] line : commandLines) {
+            if (line.length!=2) continue;
+            final String command = line[0];
+            final String desc = line[1];
+            ChatUtil.addChatMessage(new ChatComponentText(EnumChatFormatting.YELLOW + command + EnumChatFormatting.GRAY + " - " + EnumChatFormatting.AQUA + desc)
+                    .setChatStyle(new ChatStyle()
+                            .setChatHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ChatComponentText(EnumChatFormatting.GRAY + "Click to put the command in chat.")))
+                            .setChatClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, command)))
+            );
+        }
+    }
 }
