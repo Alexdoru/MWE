06f564bc4d5f5f4bd4f8e1222379cd803805e2e8
shvaich <260186867+shvaich@users.noreply.github.com>
2026-07-27 20:31:10 +0300

    added more functionality to work cleanly with the ConfigGuiScreen and to allow inheritors more control. (these additions will be used by the selector button that comes in future and the color button edit if you wish so)

--- src/configlib/java/fr/alexdoru/configlib/lib/gui/elements/OverlayConfigGuiButton.java
+++ src/configlib/java/fr/alexdoru/configlib/lib/gui/elements/OverlayConfigGuiButton.java
@@ -1,9 +1,12 @@
 package fr.alexdoru.configlib.lib.gui.elements;
 
+import fr.alexdoru.configlib.api.ColorPalette;
 import fr.alexdoru.configlib.api.ConfigProperty;
+import fr.alexdoru.configlib.lib.gui.MouseButton;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.List;
 
 public abstract class OverlayConfigGuiButton extends ConfigGuiButton {
 
@@ -13,8 +16,29 @@ public abstract class OverlayConfigGuiButton extends ConfigGuiButton {
         super(field, event, annotation);
     }
 
+    public abstract boolean isMouseOverOverlay(int mouseX, int mouseY);
+
+    public abstract void drawOverlay(ColorPalette colorPalette, int mouseX, int mouseY);
+
+    public List<String> getOverlayHoveringTextLines() { return null; }
+
+    /** @return {@code true} if the overlay handles the mouse click */
+    public abstract boolean mouseClickedOnOverlay(int mouseX, int mouseY, MouseButton button) throws IllegalAccessException;
+
+    /** @return {@code true} if the overlay handles the mouse release */
+    public boolean mouseReleasedOnOverlay(int mouseX, int mouseY, MouseButton button) { return false; }
+
+    /** @return {@code true} if the overlay handles the mouse input */
+    public boolean handleOverlayMouseInput() { return false; }
+
+    /** @return {@code true} if the overlay handles the key typed */
+    public boolean overlayKeyTyped(char typedChar, int keyCode) { return false; }
+
     public boolean isOverlayOpen() {
         return isOverlayOpen;
     }
 
+    public void closeOverlay() {
+        this.isOverlayOpen = false;
+    }
 }
