6eed8d6654273844f66538db5e1472d97c4b456b
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2026-08-30 00:25:55 +0200

    no newline

--- src/main/java/fr/alexdoru/mwe/api/MWECommandBase.java
+++ src/main/java/fr/alexdoru/mwe/api/MWECommandBase.java
@@ -61,7 +61,8 @@ public abstract class MWECommandBase extends CommandBase {
      *                     if (commandToPutOnClick) isn't provided will use (command)
      */
     protected static void printCommandHelpBlock(String header, String[][] commandLines) {
-        ChatUtil.addChatMessage(new ChatComponentText(ChatUtil.centerLine(EnumChatFormatting.GOLD + header + "\n")));
+        ChatUtil.addChatMessage(new ChatComponentText(ChatUtil.centerLine(EnumChatFormatting.GOLD + header)));
+        ChatUtil.addChatMessage("");
         for (final String[] line : commandLines) {
             if (line.length < 2) continue;
             final String command = line[0];
